fc8a30aaeabb19917f3b2141054a22e221e78601
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-06 18:50:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (44.98%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -1,16 +1,13 @@
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         
-       stack=[]
-       res=[]
-
-       while root!=None or stack!=[]:
-            while root!=None: # keep moving left until it becomes none
-                res.append(root.val)  # access the val
-                stack.append(root) #save address of last visited node
-                root=root.left #move left until left is none. # then move left
-            
-            root=stack.pop() # last visited node LIFO
-            root=root.right  # now val done and left is done # move right
-        
-       return res
+        def traversal(root):
+            #other approach
+            if root!=None:
+                res.append(root.val)
+                traversal(root.left)
+                traversal(root.right)
+        #------------------------------------
+        res=[]
+        traversal(root)
+        return res
