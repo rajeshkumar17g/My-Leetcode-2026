5d8d1f3b16d73239ee150d3574ce6b5769a0b92d
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-07 09:39:46 +0530

    Time: 4 ms (5.87%) | Memory: 20 MB (72.72%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -1,59 +1,20 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-
-        def traversal(root,res,depth):
-            if root:
-                if len(res)==depth:
-                    res.append([])
-                res[depth].append(root.val)
-                traversal(root.left,res,depth+1)
-                traversal(root.right,res,depth+1)
-        #-------------------------------------------
+        if root==None:
+            return []
         
         res=[]
-        traversal(root,res,0) # depth starts from depth=0
+        q=deque([root])
+
+        while q:
+            level=[]
+            for _ in range(len(q)):
+                node=q.popleft()
+                level.append(node.val)
+                if node.left!=None:
+                    q.append(node.left)
+                if node.right!=None:
+                    q.append(node.right)
+            res.append(level)
         return res
 
-"""class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        def traversal(root,res,depth):
-            if root:
-                if len(res)==depth:
-                    res.append([])
-                res[depth].append(root.val)
-                traversal(root.left,res,depth+1)
-                traversal(root.right,res,depth+1)
-        #-------------------------------------------
-        res=[]
-        traversal(root,res,0) # depth starts from depth=0
-        return res
-
-
-        class Solution:
-    def traversal(self,root,res,depth):
-            if root:
-                if len(res)==depth:
-                    res.append([])
-                res[depth].append(root.val)
-                self.traversal(root.left,res,depth+1)
-                self.traversal(root.right,res,depth+1)
-                
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        #-------------------------------------------
-        res=[]
-        self.traversal(root,res,0) # depth starts from depth=0
-        return res
-
-
-    class Solution:
-    def __init__(self):
-        self.res=[]
-    def levelOrder(self, root: Optional[TreeNode],depth=0) -> List[List[int]]:
-        if root:
-                if len(self.res)==depth:
-                    self.res.append([])
-                self.res[depth].append(root.val)
-                self.levelOrder(root.left,depth+1)
-                self.levelOrder(root.right,depth+1)
-        return self.res
-"""
