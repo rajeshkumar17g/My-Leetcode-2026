068eeafd4dc687a5ba1ca33afbe5ec6465e54825
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-09 09:33:44 +0530

    Time: 8 ms (82.21%) | Memory: 22.4 MB (36.06%) - LeetSync

--- 1254-deepest-leaves-sum/deepest-leaves-sum.py
+++ 1254-deepest-leaves-sum/deepest-leaves-sum.py
@@ -8,11 +8,11 @@ class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
         if root==None:
             return []
-        #FIFO
-        q=deque([root]) # q=[root]. # deque array. .append(). .popleft()
+        
         res=[]
+        q=deque([root])
 
-        while q: # while q is not empty 
+        while q:
             level=[]
             for _ in range(len(q)):
                 node=q.popleft()
@@ -22,5 +22,4 @@ class Solution:
                 if node.right!=None:
                     q.append(node.right)
             res.append(level)
-        
         return sum(res[-1])
