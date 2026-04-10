424dad9bed2e98bd2c2577b4588361b72de4b1f9
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-10 10:47:06 +0530

    Time: 2 ms (67.97%) | Memory: 20.1 MB (90.57%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -7,15 +7,14 @@ class Solution:
         res = 0
 
         while queue:
-            level = len(queue)
-            curr = []
-            for _ in range(level):
+            level = []
+            for _ in range(len(queue)):
                 node, index = queue.popleft()
-                curr.append(index)
+                level.append(index)
                 if node.left:
                     queue.append((node.left, index*2))
                 if node.right:
                     queue.append((node.right, index*2+1))
-            curr_size = curr[-1] - curr[0] + 1
+            curr_size= level[-1] - level[0] + 1
             res = max(res, curr_size)
         return res
