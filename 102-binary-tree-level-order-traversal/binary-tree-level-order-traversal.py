3ee5bc945bba69266dfc0d4b086ed5aa1219f87a
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-10 14:24:36 +0530

    Time: 3 ms (18.92%) | Memory: 20 MB (38.21%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -4,12 +4,16 @@ class Solution:
         def dfs(root,depth):
             if root==None:
                 return
-            if len(res)==depth:
-                res.append([]) # 1st time when we reach a level we add []
+            
+            if depth==len(res):
+                res.append([])
+            
             res[depth].append(root.val)
             dfs(root.left,depth+1)
             dfs(root.right,depth+1)
-        #----------------------------------
+        #-----------------------------------
         res=[]
-        dfs(root,0) #depth=0
+        dfs(root,0)
         return res
+
+
