a7614c20914d803bebcf0df676c10679516dad60
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-11 16:32:29 +0530

    Time: 57 ms (61.04%) | Memory: 20.6 MB (87.06%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -1,16 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
 class Solution:
-    def connect(self, root):
-        if not root or not root.left:
-            return root
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         
-        # connect siblings
-        root.left.next = root.right
-        
-        # connect across parents
-        if root.next:
-            root.right.next = root.next.left
-        
-        self.connect(root.left)
-        self.connect(root.right)
-        
+        def dfs(root):
+            if root==None:
+                return
+            if root.left!=None:
+                root.left.next=root.right
+            if root.right!=None and root.next!=None:
+                root.right.next=root.next.left
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
         return root
