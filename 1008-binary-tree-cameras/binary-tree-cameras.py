941bbe5188b362cec2800e045efcaa0355873b7d
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-04-13 12:04:22 +0530

    Time: 4 ms (21.06%) | Memory: 19.4 MB (90.50%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.py
+++ 1008-binary-tree-cameras/binary-tree-cameras.py
@@ -3,7 +3,7 @@ class Solution:
         self.count=0
         def dfs(root):
             if root==None:
-                return None
+                return 
             if root.left==None and root.right==None:
                 return 'leaf'
 
@@ -13,13 +13,12 @@ class Solution:
             if left=='leaf' or right=='leaf':
                 self.count+=1
                 return 'camera'
-
             if left=='camera' or right=='camera':
                 return 'gap'
-
-            if left=='gap' or right=='gap':
-                return 'leaf'
-        if dfs(root)=='leaf':
-            return self.count+1 # leaf
+            if left=="gap" or right=="gap":
+                return "leaf"
+            
+        #--------------------------------------
+        if dfs(root)=="leaf":
+            return self.count+1
         return self.count
-
