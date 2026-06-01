b80448e21710888a3df9800d4ce79fd52adf6c77
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-01 14:11:30 +0530

    Time: 3 ms (21.24%) | Memory: 19.3 MB (51.14%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,7 +1,13 @@
 class Solution:
     def checkIfPangram(self, s: str) -> bool:
     
+        arr=[0]*26
+        for ch in s:
+            arr[ord(ch)%97]=1
+        return sum(arr)==26
+        '''
         for unicode in range(97,123):
             if chr(unicode) not in s:
                 return False
         return True
+        '''
