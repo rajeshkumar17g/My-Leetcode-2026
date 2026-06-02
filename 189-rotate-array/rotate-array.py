1e5df2d341109e675a1978efa68a44db19b4ba6c
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-02 11:37:30 +0530

    Time: 3 ms (75.85%) | Memory: 26.4 MB (70.60%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,5 +3,5 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for _ in range(k):
-            nums.insert(0,nums.pop())
+        k=k%len(nums)
+        nums[::]=nums[-k:]+nums[:-k]
