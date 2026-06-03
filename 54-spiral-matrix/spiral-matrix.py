e7b6858208812af7c50c217812ee433ef61d5a49
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-03 14:39:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (94.31%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,30 +1,34 @@
 class Solution:
-    def spiralOrder(self, matrix):
-        res = []
-        top, bottom = 0, len(matrix) - 1
-        left, right = 0, len(matrix[0]) - 1
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        n=len(matrix)
+        m=len(matrix[0])
 
-        while top <= bottom and left <= right:
-            # Top row
-            for i in range(left, right + 1):
+        top=0
+        bottom=n-1
+        left=0
+        right=m-1
+        res=[]
+
+        while top<=bottom and left<=right:
+            #left to right
+            for i in range(left,right+1):
                 res.append(matrix[top][i])
-            top += 1
+            top+=1
 
-            # Right column
-            for i in range(top, bottom + 1):
+            # top -> bottom
+            for i in range(top,bottom+1):
                 res.append(matrix[i][right])
-            right -= 1
+            right-=1
 
-            if top <= bottom:
-                # Bottom row
-                for i in range(right, left - 1, -1):
+            # right to left
+            if top<=bottom:
+                for i in range(right,left-1,-1):
                     res.append(matrix[bottom][i])
-                bottom -= 1
+                bottom-=1
 
-            if left <= right:
-                # Left column
-                for i in range(bottom, top - 1, -1):
+            # bottom to top
+            if left<=right:
+                for i in range(bottom,top-1,-1):
                     res.append(matrix[i][left])
-                left += 1
-                
+                left+=1
         return res
