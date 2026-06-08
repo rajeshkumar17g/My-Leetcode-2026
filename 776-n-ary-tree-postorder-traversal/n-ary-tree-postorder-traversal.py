4d5d110c90e2699cd25170f7bca33a037c17371f
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-08 10:52:37 +0530

    Time: 59 ms (27.94%) | Memory: 21.1 MB (21.21%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -1,13 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
         def traversal(root):
             if root==None:
                 return
-            
             for child in root.children:
                 traversal(child)
             res.append(root.val)
-        #---------------------------
+        #---------------------------------------------------------
         res=[]
         traversal(root)
         return res
