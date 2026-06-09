cb108d3e702db033097cb8b41f50cdfeb4094b49
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-09 13:13:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (28.89%) - LeetSync

--- 1005-univalued-binary-tree/univalued-binary-tree.py
+++ 1005-univalued-binary-tree/univalued-binary-tree.py
@@ -1,21 +1,13 @@
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
-        
+
         def traversal(root):
             if root==None:
                 return
-            res.add(root.val)
+            res.append(root.val)
             traversal(root.left)
             traversal(root.right)
-        #----------------------
-        res=set()
+        #----------------------------
+        res=[]
         traversal(root)
-        return len(res)==1
-        """
-        if root==None:
-            return True
-        if root.left!=None and root.val!=root.left.val:
-            return False
-        if root.right!=None and root.val!=root.right.val:
-            return False
-        return self.isUnivalTree(root.left) and self.isUnivalTree(root.right)"""
+        return len(set(res))==1
