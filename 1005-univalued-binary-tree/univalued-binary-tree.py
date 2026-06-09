f5ca8e13006ee3119a7266b03b390f318c0efeea
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-09 13:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (28.89%) - LeetSync

--- 1005-univalued-binary-tree/univalued-binary-tree.py
+++ 1005-univalued-binary-tree/univalued-binary-tree.py
@@ -1,6 +1,17 @@
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
 
+        if root==None:
+            return True
+        elif root.left!=None and root.val!=root.left.val:
+            return False
+        elif root.right!=None and root.val!=root.right.val:
+            return False
+        return self.isUnivalTree(root.left) and self.isUnivalTree(root.right)
+
+
+        '''
+
         def traversal(root):
             if root==None:
                 return
@@ -11,3 +22,4 @@ class Solution:
         res=[]
         traversal(root)
         return len(set(res))==1
+        '''
