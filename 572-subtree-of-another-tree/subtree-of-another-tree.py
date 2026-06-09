b86b84aebfcbfcdcdaf13eff097248c2683684f0
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-09 10:53:25 +0530

    Time: 51 ms (13.85%) | Memory: 19.4 MB (97.70%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -8,7 +8,7 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if p==None and q==None:
             return True
-        if p==None or q==None or p.val!=q.val:
+        elif p==None or q==None or p.val!=q.val:
             return False
         return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
 
@@ -18,4 +18,3 @@ class Solution:
         if self.isSameTree(root,subRoot):
             return True
         return self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
-        
