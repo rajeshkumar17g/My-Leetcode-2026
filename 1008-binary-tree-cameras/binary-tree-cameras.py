00d66ab368089cb8ccdfaada250a1624d0f4e4dc
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-10 09:25:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (57.37%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.py
+++ 1008-binary-tree-cameras/binary-tree-cameras.py
@@ -1,25 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def minCameraCover(self, root: Optional[TreeNode]) -> int:
         self.count=0
-        def dfs(root):
+        def traversal(root):
             if root==None:
-                return 
+                return
             if root.left==None and root.right==None:
                 return 'leaf'
-
-            left=dfs(root.left)
-            right=dfs(root.right)
+            left=traversal(root.left)
+            right=traversal(root.right)
 
             if left=='leaf' or right=='leaf':
                 self.count+=1
                 return 'camera'
             if left=='camera' or right=='camera':
                 return 'gap'
-            if left=='gap' or right=="gap":
-                return "leaf"
-            
-            
-        #--------------------------------------
-        if dfs(root)=='leaf':
-            return self.count+1
+            if left=='gap' or right=='gap':
+                return 'leaf'
+        #---------------------------------
+        if traversal(root)=='leaf':
+            self.count+=1
         return self.count
