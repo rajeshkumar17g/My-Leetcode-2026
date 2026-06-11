ca4c7596d7924566afb626c3558d36ca2cf74d19
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-11 15:41:52 +0530

    Time: 572 ms (5.01%) | Memory: 41.2 MB (5.30%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -4,6 +4,29 @@ class Solution:
             if i==len(nums): # if i==len means all swappings are done thats a solution
                 res.append(nums[:])# storing a copy of it
                 return # we reached a solution, so stop 
+
+            for j in range(i,len(nums)):
+                nums[i],nums[j]=nums[j],nums[i] # made a choice
+                backtracking(nums,i+1) # tracking the new path
+                nums[i],nums[j]=nums[j],nums[i] # undo the choice
+        #----------------------------------------------------------------------
+        res=[]
+        backtracking(nums,0)
+        ans=[]
+        for l in res:
+            if l not in ans:
+                ans.append(l)
+        return ans
+
+
+'''
+BACKTRACKING OR OPTIMIZED SOLUTION
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def backtracking(nums,i):
+            if i==len(nums): # if i==len means all swappings are done thats a solution
+                res.append(nums[:])# storing a copy of it
+                return # we reached a solution, so stop 
             hash=set()
             for j in range(i,len(nums)):
                 if nums[j] in hash:
@@ -16,3 +39,4 @@ class Solution:
         res=[]
         backtracking(nums,0)
         return res
+'''
