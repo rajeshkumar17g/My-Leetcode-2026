d7f9225f2afbbb74835e24e7250ed71f21e8a2ef
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-19 10:16:03 +0530

    Time: 19 ms (6.74%) | Memory: 19.7 MB (54.91%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,6 +3,32 @@ class Solution:
         s=s.lower()
         res=""
         for ch in s:
+            if( (ch>='A' and ch<='Z') or 
+                (ch>='a' and ch<='z') or 
+                (ch>='0' and ch<='9')):
+                        res=res+ch
+        left=0
+        right=len(res)-1
+        while left<=right:
+            if res[left]!=res[right]:
+                return False
+            left=left+1
+            right=right-1
+        return True
+        
+
+
+
+
+
+
+        '''
+
+        s=s.lower()
+        res=""
+        for ch in s:
             if ch.isalnum():
                 res=res+ch
         return res==res[::-1]
+
+        '''
