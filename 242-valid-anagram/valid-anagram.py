68341174278e190b9cf8dd34df32a168f5efac28
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-06-19 10:57:31 +0530

    Time: 7 ms (91.18%) | Memory: 19.3 MB (94.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        arr1=[0]*26
+        hashtable=[0]*26
         for ch in s:
-            index=ord(ch)%97
-            arr1[index]+=1
-
-        arr2=[0]*26
+            index=ord(ch)-97
+            hashtable[index]+=1
         for ch in t:
-            index=ord(ch)%97
-            arr2[index]+=1
+            index=ord(ch)-97
+            hashtable[index]-=1
         
-        return arr1==arr2
+        for ele in hashtable:
+            if ele!=0:
+                return False
+        return True
