f356a499ed68687707e5feb8d9e4267e765f80b4
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-01 14:11:34 +0530

    Time: 75 ms (6.96%) | Memory: 37.8 MB (98.26%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,27 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        slow=head
-        fast=head
+        slow=fast=head
         while fast!=None and fast.next!=None:
             slow=slow.next
             fast=fast.next.next
         
-        rev_head=None
         crr=head
+        rev_head=None
         while crr!=slow:
             after=crr.next
             crr.next=rev_head
             rev_head=crr
             crr=after
         
+
+
         if fast!=None:
             slow=slow.next
+        
 
-        crr1=rev_head
-        crr2=slow
+        crr1=slow
+        crr2=rev_head
         while crr1!=None and crr2!=None:
             if crr1.val!=crr2.val:
                 return False
             crr1=crr1.next
             crr2=crr2.next
         return True
+        
+
+
