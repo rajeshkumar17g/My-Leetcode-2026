2f30b35b05c3ba4dd5a300d1ec24b83e010309b3
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-07 16:26:45 +0530

    Time: 2 ms (99.27%) | Memory: 44.7 MB (46.56%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,5 +1,34 @@
 class Solution {
     public boolean isPalindrome(String s) {
+
+        int left=0;
+        int right=s.length()-1;
+        while(left<=right){
+            while( left<=right && !(s.charAt(left)>=65 && s.charAt(left)<=90 || 
+                  s.charAt(left)>=97 && s.charAt(left)<=122 ||
+                  s.charAt(left)>=48 && s.charAt(left)<=57)){
+                    left++;
+                  }
+             while( left<=right && !(s.charAt(right)>=65 && s.charAt(right)<=90 || 
+                  s.charAt(right)>=97 && s.charAt(right)<=122 ||
+                  s.charAt(right)>=48 && s.charAt(right)<=57)){
+                    right--;
+                  }
+            if(left<=right && Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+
+
+
+
+
+
+
+       /*
         s=s.toLowerCase();
         String res="";
         for(int i=0;i<s.length();i++){
@@ -9,12 +38,6 @@ class Solution {
                     res=res+s.charAt(i);
               }
         }
-
-        System.out.println(res);
-         System.out.println(res);
-          System.out.println(res);
-           System.out.println(res);
-            System.out.println(res);
         int left=0,right=res.length()-1;
         while(left<=right){
             if(res.charAt(left)!=res.charAt(right)){
@@ -23,7 +46,7 @@ class Solution {
             left++;
             right--;
         }
-        return true;
+        return true;*/
 
 
 
