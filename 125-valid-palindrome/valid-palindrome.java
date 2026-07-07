3f484307023c6936239006fa61c37a9d894f8407
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-07 16:12:14 +0530

    Time: 129 ms (5.49%) | Memory: 48.3 MB (5.23%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        String res="";
+        for(int i=0;i<s.length();i++){
+            if((s.charAt(i)>=65 && s.charAt(i)<=90) ||
+              (s.charAt(i)>=97 && s.charAt(i)<=122) ||
+              (s.charAt(i)>=48 && s.charAt(i)<=57)){
+                    res=res+s.charAt(i);
+              }
+        }
+
+        System.out.println(res);
+        int left=0,right=res.length()-1;
+        while(left<=right){
+            if(res.charAt(left)!=res.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
