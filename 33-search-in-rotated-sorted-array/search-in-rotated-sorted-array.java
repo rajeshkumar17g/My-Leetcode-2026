7e6c74daa87af4987a4105d92d81e54c4be7a03d
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-07 10:57:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (91.87%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -9,7 +9,7 @@ class Solution {
                 return mid;
             }
             else if(nums[low]<=nums[mid]){
-                if(target>=nums[low] && target<nums[mid]){
+                if(target>=nums[low] && target<=nums[mid]){
                     high=mid-1;
                 }
                 else{
@@ -17,7 +17,7 @@ class Solution {
                 }
             }
             else{
-                if(target>=nums[mid] && target<=nums[high]){
+                if(target>nums[mid] && target<=nums[high]){
                    low=mid+1;
                 }
                 else{
