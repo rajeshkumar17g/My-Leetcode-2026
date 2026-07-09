df52cce88137e250971cfad56fc6f8527069ee70
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-09 12:11:10 +0530

    Time: 2 ms (98.60%) | Memory: 46.9 MB (19.77%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -15,6 +15,39 @@ class Solution {
            for(int k=0;k<nums.length;k++){
             sol.add(nums[k]);
            }
+            res.add(sol);// solution state adda deepcopy
+            return;
+        }
+        HashSet<Integer> seen=new HashSet<>();
+        for(int j=i;j<nums.length;j++){
+            if(seen.contains(nums[j])){
+                continue;
+            }
+            seen.add(nums[j]);
+            swap(nums,i,j); // make a choice # swap
+            backtracking(nums,i+1,res);//explore this path
+            swap(nums,i,j);// undo the choice # unswap
+        }
+    }
+}
+/*
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        backtracking(nums,0,res);
+        return res;
+    }
+    private void swap(int[] nums,int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    private void backtracking(int[] nums, int i, List<List<Integer>> res){
+        if(i==nums.length){
+            ArrayList<Integer> sol=new ArrayList<>();
+           for(int k=0;k<nums.length;k++){
+            sol.add(nums[k]);
+           }
            if(!res.contains(sol)){
             res.add(sol);// solution state adda deepcopy
            }
@@ -26,4 +59,4 @@ class Solution {
             swap(nums,i,j);// undo the choice # unswap
         }
     }
-}
+}*/
