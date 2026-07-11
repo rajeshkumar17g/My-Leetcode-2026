f7046fdb731c255d81f550387d3c84f9b11a27fe
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-11 20:10:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (91.99%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,16 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
-        def helper(subset,i):
-            if i==len(nums):
-                res.append(subset[:])
+       def backtracking(i,subset):
+            if(i==len(nums)): # curr_state==sol_state
+                res.append(subset[:]) # adding a copy of subset
                 return
-            
-            subset.append(nums[i]) # pick made a move
-            helper(subset,i+1) # backtracking # exploring that path
-            subset.pop() # undo the move # Not pick
-            helper(subset,i+1)
-        #-----------------------------
-        res=[]
-        helper([],0) #subset=[]. i=0
-        return res
+            subset.append(nums[i]) # make the move # pick
+            backtracking(i+1,subset) # backtrack
+            subset.pop() # undo the move  # noty pick
+            backtracking(i+1,subset) # backtrack 
+       #----------------------------------------
+       res=[]
+       backtracking(0,[]) # i=0 subset=[]
+       return res
