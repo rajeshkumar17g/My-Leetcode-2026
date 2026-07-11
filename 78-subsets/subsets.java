006a682b7eb0dd5c96cd1fc4e2c217b7924a889a
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-11 20:35:36 +0530

    Time: 1 ms (87.27%) | Memory: 44.1 MB (57.72%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,46 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res= new ArrayList<>();
-        ArrayList<Integer> subset= new ArrayList<>();
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> subset=new ArrayList<>();
         backtracking(nums,0,subset,res);
         return res;
     }
-    private void backtracking(int[] nums, int i, ArrayList<Integer> subset, List<List<Integer>> res){
-
-        
-        res.add(new ArrayList<>(subset)); // every state is a solution state
-        for(int j=i;j<nums.length;j++){
-            subset.add(nums[j]); // pick // make a choice
-            backtracking(nums,j+1,subset,res); // backtrack // explore a new path
-            subset.remove(subset.size()-1); // not pick // undo the choice
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-    /*
-     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res= new ArrayList<>();
-        ArrayList<Integer> subset= new ArrayList<>();
-        backtracking(nums,0,subset,res);
-        return res;
-    }
-    private void backtracking(int[] nums, int i, ArrayList<Integer> subset, List<List<Integer>> res){
-
+    private void backtracking(int[] nums,int i,  List<Integer> subset,  List<List<Integer>> res){
         if(i==nums.length){
-            res.add(new ArrayList<>(subset));// adding a copy of subset
+            res.add(new ArrayList<>(subset));
             return;
         }
-        subset.add(nums[i]); // Pick making a choice
-        backtracking(nums,i+1,subset,res); // exploring a path
-        subset.remove(subset.size()-1);// not picking # undo the choice
-        backtracking(nums,i+1,subset,res); // exploring a path
-    }*/
+        subset.add(nums[i]);
+        backtracking(nums,i+1,subset,res);
+        subset.remove(subset.size()-1); 
+        backtracking(nums,i+1,subset,res);
+    }
 }
