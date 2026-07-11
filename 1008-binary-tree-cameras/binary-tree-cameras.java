579081d7e782b36c31040586aba77183f5e45db3
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-11 15:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (62.50%) - LeetSync

---
+++ 1008-binary-tree-cameras/binary-tree-cameras.java
@@ -0,0 +1,31 @@
+class Solution {
+    int cameras=0;
+    public int minCameraCover(TreeNode root) {
+        if(traversal(root).equals("leaf")){
+            return cameras+1;
+        };
+        return cameras;
+    }
+    private String traversal(TreeNode root){
+        if(root==null){
+            return "";
+        }
+        if(root.left==null && root.right==null){
+            return "leaf";
+        }
+
+        String left=traversal(root.left);
+        String right=traversal(root.right);
+
+        if(left.equals("leaf") || right.equals("leaf")){
+            cameras++;
+            return "camera";
+        }
+        else if(left.equals("camera") || right.equals("camera")){
+            return "gap";
+        }
+        else{
+            return "leaf";
+        }
+    }
+}
