e94e9a88d9cfd4a961b1a8b51fcf9aaa4b7e21ef
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-12 20:21:59 +0530

    Time: 26 ms (25.91%) | Memory: 19.7 MB (33.49%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,33 +1,29 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def convert(board):
-            sol=[]
-            for col in board:  #[1,3,0,2]
-                row=['.']*n
-                row[col]='Q' # ['.','Q','.','.']
-                sol.append(''.join(row))
-            return sol
-
-
         def is_valid(x2,y2,board):
             for x1 in range(x2):
-                if y2==board[x1] or abs(x1-x2)==abs(board[x1]-y2):
-                    return False # not a valid placement
-            return True 
-        #------------------------------------------
-        def backtracking(board,row):
+                y1=board[x1]
+                if(y1==y2 or abs(x1-x2)==abs(y1-y2)):
+                    return False
+            return True
+        #-------------------------------------------------
+        def backtrack(row,board):
             if row==n:
-                res.append(convert(board[:])) # adds a copy of board
+                sol=[]
+                for col in board:
+                    row_list=['.']*n
+                    row_list[col]='Q'
+                    sol.append(''.join(row_list))
+                    
+                res.append(sol)
                 return
             
-            for col in range(0,n): # col => 0 ,1,.... n-1
-                if is_valid(row,col,board):
-                    board[row]=col # make the move
-                    backtracking(board,row+1) # explore the path
-                    board[row]=-1 # undo the move
-
-        #---------------------------------------
-        res=[]                     #.row    0. 1. 2. 3.  ==> board[row]=> col
-        board=[-1]*n # n=4.   board=[-1,-1,-1,-1] # col locations of Queens
-        backtracking(board,0) # row=0. board=[-1,-1,-1,-1]
+            for col in range(n):
+                if(is_valid(row,col,board)):
+                    board[row]=col
+                    backtrack(row+1,board)
+                    board[row]=-1
+        res=[]
+        board=[-1]*n # empty board [-1,-1,-1,-1]
+        backtrack(0,board)
         return res
