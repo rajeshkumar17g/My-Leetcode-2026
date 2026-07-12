f63e60be97dd924eebfab1306c4b7b44b655c9bf
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-12 18:45:39 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (29.38%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -9,19 +9,19 @@ class Solution {
         nums[i]=nums[j];
         nums[j]=temp;
     }
-    private void backtracking(int[] nums, int i, List<List<Integer>> res){
+    private void backtracking(int[] nums,int i,  List<List<Integer>> res){
         if(i==nums.length){
-            ArrayList<Integer> sol=new ArrayList<>();
-           for(int k=0;k<nums.length;k++){
-            sol.add(nums[k]);
-           }
-            res.add(sol);// solution state adda deepcopy
+            List<Integer> copy=new ArrayList<>();
+            for(int ele: nums){
+                copy.add(ele);
+            }
+            res.add(copy);
             return;
         }
         for(int j=i;j<nums.length;j++){
-            swap(nums,i,j); // make a choice # swap
-            backtracking(nums,i+1,res);//explore this path
-            swap(nums,i,j);// undo the choice # unswap
+            swap(nums,i,j);
+            backtracking(nums,i+1,res);
+            swap(nums,i,j);
         }
     }
 }
