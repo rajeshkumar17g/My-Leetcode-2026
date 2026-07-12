0b8c043511313fd92bab0d5f0eab97f5b2712ede
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-12 19:26:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (88.14%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,20 +1,17 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-       def backtracking(i,subset):
+        def backtracking(i,subset):
             if(i==len(nums)): # curr_state==sol_state
                 res.append(subset[:]) # adding a copy of subset
                 return
             subset.append(nums[i]) # make the move # pick
             backtracking(i+1,subset) # backtrack
+            while(i+1<len(nums) and nums[i]==nums[i+1]):
+                i=i+1
             subset.remove(nums[i]) # undo the move  # noty pick
             backtracking(i+1,subset) # backtrack 
        #----------------------------------------
-       res=[]
-       nums.sort()
-       backtracking(0,[]) # i=0 subset=[]
-       ans=[]
-
-       for subset in res:
-        if subset not in ans:
-            ans.append(subset)
-       return ans
+        res=[]
+        nums.sort()
+        backtracking(0,[]) # i=0 subset=[]
+        return res
