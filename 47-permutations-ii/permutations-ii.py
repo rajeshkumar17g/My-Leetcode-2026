12f63fde5d4b027250162df7b81de45aa03c82f5
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-12 19:06:27 +0530

    Time: 591 ms (5.04%) | Memory: 41.4 MB (5.49%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,42 +1,18 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        def backtracking(nums,i):
-            if i==len(nums): # if i==len means all swappings are done thats a solution
-                res.append(nums[:])# storing a copy of it
-                return # we reached a solution, so stop 
-
+        def backtracking(i):
+            if i==len(nums):
+                res.append(nums[:])
+                return
             for j in range(i,len(nums)):
-                nums[i],nums[j]=nums[j],nums[i] # made a choice
-                backtracking(nums,i+1) # tracking the new path
-                nums[i],nums[j]=nums[j],nums[i] # undo the choice
-        #----------------------------------------------------------------------
+                nums[i],nums[j]=nums[j],nums[i]
+                backtracking(i+1)
+                nums[i],nums[j]=nums[j],nums[i]
+        #----------------------------------------------------
         res=[]
-        backtracking(nums,0)
+        backtracking(0)   # i=0
         ans=[]
-        for l in res:
-            if l not in ans:
-                ans.append(l)
+        for li in res:
+            if li not in ans:
+                ans.append(li)
         return ans
-
-
-'''
-BACKTRACKING OR OPTIMIZED SOLUTION
-class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        def backtracking(nums,i):
-            if i==len(nums): # if i==len means all swappings are done thats a solution
-                res.append(nums[:])# storing a copy of it
-                return # we reached a solution, so stop 
-            hash=set()
-            for j in range(i,len(nums)):
-                if nums[j] in hash:
-                    continue
-                hash.add(nums[j])
-                nums[i],nums[j]=nums[j],nums[i] # made a choice
-                backtracking(nums,i+1) # tracking the new path
-                nums[i],nums[j]=nums[j],nums[i] # undo the choice
-        #----------------------------------------------------------------------
-        res=[]
-        backtracking(nums,0)
-        return res
-'''
