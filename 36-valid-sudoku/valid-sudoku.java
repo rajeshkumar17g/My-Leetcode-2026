5bdf5ce9cf5fa4d72aabd79a53079b848996cacf
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-14 18:27:29 +0530

    Time: 1 ms (99.95%) | Memory: 46.6 MB (46.60%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,5 +1,24 @@
 class Solution {
+    private boolean is_valid(char[][] board,int row,int col){
+        int num=board[row][col];
+        for(int i=0;i<9;i++){
+            if((col!=i && num==board[row][i]) || (row!=i && num==board[i][col])){
+                return false;
+            }   
+        }
+        int rs=(row/3)*3;
+            int cs=(col/3)*3;
+            for(int r=rs;r<rs+3;r++){
+                for(int c=cs;c<cs+3;c++){
+                    if(num==board[r][c] && (row!=r && col!=c)){
+                        return false;
+                    }
+                }
+            }
+            return true;
+    }
     public boolean isValidSudoku(char[][] board) {
+
         for(int row=0;row<9;row++){
             for(int col=0;col<9;col++){
                 if(board[row][col]!='.'){
@@ -9,49 +28,7 @@ class Solution {
                 }
             }
         }
-        return true;
-    }
-    private boolean is_valid(char[][] board, int row,int col){
-
-        // checking row
-        for(int i=0;i<9;i++){
-            // row is fixed 
-            if(col!=i && board[row][col]==board[row][i]){
-                return false;
-            }
-        }
-
-          // checking col
-        for(int i=0;i<9;i++){
-            // colis fixed 
-            if(row!=i && board[row][col]==board[i][col]){
-                return false;
-            }
-        }
-
-        //checking the same grid
-        int r=(row/3)*3;
-        int c=(col/3)*3;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if((row!=r+i && col!=c+j) && board[row][col]==board[r+i][c+j]){
-                    return false;
-                }
-            }
-        }
-
 
         return true;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
