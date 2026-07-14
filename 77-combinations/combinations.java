55b8f56559e775b935ccb5e80e458a6e8f1f55ab
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-14 19:07:52 +0530

    Time: 23 ms (16.14%) | Memory: 98 MB (46.74%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,32 +1,25 @@
 class Solution {
-    public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> res=new ArrayList<>();
-        ArrayList<Integer> subset=new ArrayList<>();
-        backtracking(1,n,k,subset,res);
-        return res;
-    }
-    private void backtracking(int i,int n, int k,ArrayList<Integer> subset,List<List<Integer>> res){
+    private void backtracking(int i,int n,int k,List<Integer> subset, List<List<Integer>> res){
         if(subset.size()>k){
             return;
         }
-        if(subset.size()==k){
+        if(k==subset.size()){
             res.add(new ArrayList<>(subset));
-            return;
         }
         for(int j=i;j<=n;j++){
-            subset.add(j);// making the choice
+            subset.add(j);
             backtracking(j+1,n,k,subset,res);
             subset.remove(subset.size()-1);
         }
+
+    }
+    public List<List<Integer>> combine(int n, int k) {
+
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> subset=new ArrayList<>();
+
+        backtracking(1,n,k,subset,res);
+
+        return res;
     }
 }
-
-
-
-
-
-
-
-
-
-
