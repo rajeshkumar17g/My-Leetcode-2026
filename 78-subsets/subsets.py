2af8a4f0b1b7f947c0b52d0b3ab685f4911562e7
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-14 18:58:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.53%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,16 +1,30 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-       def backtracking(i,subset):
-            if(i==len(nums)): # curr_state==sol_state
-                res.append(subset[:]) # adding a copy of subset
-                return
-            subset.append(nums[i]) # make the move # pick
-            backtracking(i+1,subset) # backtrack
-            subset.remove(nums[i]) # undo the move  # noty pick
-            backtracking(i+1,subset) # backtrack 
-       #----------------------------------------
-       res=[]
-       backtracking(0,[]) # i=0 subset=[]
-       return res
+        def backtracking(i,subset):
+            res.append(subset[:])
+            for j in range(i,len(nums)):
+                subset.append(nums[j])
+                backtracking(j+1,subset)
+                subset.pop()
+        #---------------------------------------------
+        res=[]
+        backtracking(0,[])
+        return res
 
-       nums=[a,b,c]
+
+
+
+        '''
+        def backtracking(i,subset):
+                if(i==len(nums)): # curr_state==sol_state
+                    res.append(subset[:]) # adding a copy of subset
+                    return
+                subset.append(nums[i]) # make the move # pick
+                backtracking(i+1,subset) # backtrack
+                subset.remove(nums[i]) # undo the move  # noty pick
+                backtracking(i+1,subset) # backtrack 
+        #----------------------------------------
+        res=[]
+        backtracking(0,[]) # i=0 subset=[]
+        return res
+        '''
