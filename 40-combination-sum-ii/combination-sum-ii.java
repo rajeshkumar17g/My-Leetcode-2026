489ff17af8d791bfcb02e3d20808b56ece2e64cc
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-14 19:38:45 +0530

    Time: 14 ms (16.89%) | Memory: 52.6 MB (5.17%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,12 +1,5 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] nums, int target) {
-        Arrays.sort(nums);
-        List<List<Integer>> res=new ArrayList<>();
-        ArrayList<Integer> subset=new ArrayList<>();
-        backtracking(nums,0,0,target,subset,res);
-        return res;
-    }
-    private void backtracking(int[] nums,int i,int sum,int target, ArrayList<Integer> subset, List<List<Integer>>  res){
+     private void backtracking( List<List<Integer>> res, List<Integer> subset, int i,int sum,int[] candidates,int target){
         if(sum==target){
             res.add(new ArrayList<>(subset));
             return;
@@ -14,15 +7,24 @@ class Solution {
         if(sum>target){
             return;
         }
-        HashSet<Integer> seen=new HashSet<Integer>();
-        for(int j=i;j<nums.length;j++){
-            if(seen.contains(nums[j])){
+        HashSet<Integer> seen=new HashSet<>();
+        for(int j=i;j<candidates.length;j++){
+            if(seen.contains(candidates[j])==true){
                 continue;
             }
-            seen.add(nums[j]);
-            subset.add(nums[j]); // making a choice
-            backtracking(nums,j+1,sum+nums[j],target,subset,res);
+            seen.add(candidates[j]);
+            subset.add(candidates[j]);
+            backtracking(res,subset,j+1,sum+candidates[j],candidates,target);
             subset.remove(subset.size()-1);
         }
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> subset=new ArrayList<>();
+        Arrays.sort(candidates);
+        backtracking(res,subset,0,0,candidates,target);
+        return res;
+
+
     }
 }
