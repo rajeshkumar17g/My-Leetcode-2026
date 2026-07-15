1b3c8cd3ebcba3c467020c32b755b8f5a322d5f6
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-15 18:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.97%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -4,38 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-'''
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if root==None:
             return []
-            
-        res=[]
-        q=deque([root]) # q=[None]
 
-        while q: 
+        q=deque([root])
+        res=[]
+
+        while q:
             level=[]
             for _ in range(len(q)):
                 node=q.popleft()
                 level.append(node.val)
+
                 if node.left!=None:
                     q.append(node.left)
                 if node.right!=None:
                     q.append(node.right)
             res.append(level[-1])
-        return res'''
-class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-
-
-        def traversal(root,depth):
-            if root==None:
-                return
-
-            if depth==len(res):
-                res.append(root.val)
-                
-            traversal(root.right,depth+1)
-            traversal(root.left,depth+1)
-        #----------------------------------------
-        res=[]
-        traversal(root,0)
         return res
