3306d3915265ff7c67fd14fec85869909e98b68d
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-15 18:52:41 +0530

    Time: 183 ms (32.10%) | Memory: 67.1 MB (81.91%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
@@ -8,21 +8,21 @@ class Solution:
     def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
         if root==None:
             return []
-            
-        res=[]
-        q=deque([root]) # q=[None]
 
-        while q: 
+        q=deque([root])
+        res=[]
+
+        while q:
             level=[]
             for _ in range(len(q)):
                 node=q.popleft()
                 level.append(node.val)
+
                 if node.left!=None:
                     q.append(node.left)
                 if node.right!=None:
                     q.append(node.right)
             res.append(sum(level))
         res.sort()
-        if k<=len(res):
-             return res[-k]
-        return -1
+        if(k>len(res)):return -1
+        return res[-k]
