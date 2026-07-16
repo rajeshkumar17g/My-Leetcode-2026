33f3d1f337af17bd2b570903cf2847d58c701d38
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-16 18:46:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (60.05%) - LeetSync

--- 1005-univalued-binary-tree/univalued-binary-tree.java
+++ 1005-univalued-binary-tree/univalued-binary-tree.java
@@ -4,10 +4,7 @@ class Solution {
         if(root==null){
             return true;
         }
-        if(root.left!=null && root.val!=root.left.val){
-            return false;
-        }
-        if(root.right!=null && root.val!=root.right.val){
+        if((root.left!=null && root.left.val!=root.val) || (root.right!=null && root.right.val!=root.val)){
             return false;
         }
         return isUnivalTree(root.left) && isUnivalTree(root.right);
