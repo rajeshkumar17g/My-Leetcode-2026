16a02b6a166ecd448516b58e9b0c367748818b8b
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-16 20:35:30 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (73.49%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -23,13 +23,11 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if(root==null){
+        if(root==null || (root.left==null && root.right==null)){
             return root;
         }
-        if(root.left!=null){
-             root.left.next=root.right;
-        }
-        if(root.next!=null && root.right!=null){
+        root.left.next=root.right;
+        if(root.next!=null){
             root.right.next=root.next.left;
         }
         connect(root.left);
