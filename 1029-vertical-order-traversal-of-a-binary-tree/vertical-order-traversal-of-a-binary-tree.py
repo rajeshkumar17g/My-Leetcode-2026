a6de7f70097558582a7085b5bbca63114115c13b
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-17 18:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (55.14%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -1,26 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def verticalTraversal(self, root):
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         def dfs(root,row,col):
-            if root==None:
+            if(root==None):
                 return
-            nodes.append([col,row,root.val])
+            res.append([col,row,root.val])
             dfs(root.left,row+1,col-1)
             dfs(root.right,row+1,col+1)
-        #-----------------------------------
-        nodes=[]
-        dfs(root,0,0)
-        nodes.sort()
-        #[[-1, 1, 9], [0, 0, 3], [0, 2, 15], [1, 1, 20], [2, 2, 7]]
+        #--------------------------------------------
         res=[]
-
-        prev=None
-
-        for col,row,val in nodes:
-            if prev==None or prev!=col:
-                res.append([])
-            res[-1].append(val)
-            prev=col
-        return res
-
-
+        dfs(root,0,0)
         
+        res.sort()
+
+        ans=[]
+        prev_col=None
+        for arr in res:  # arr[0]=col  arr[1]=row  # arr[2]=val
+            if  prev_col==None or prev_col!=arr[0]:
+                ans.append([])
+            ans[-1].append(arr[2])
+            prev_col=arr[0]
+        return ans
