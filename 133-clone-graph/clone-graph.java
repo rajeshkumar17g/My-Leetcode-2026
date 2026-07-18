6f5d34a5021f4af997e03e54533aa5e674e7453b
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-18 18:49:28 +0530

    Time: 26 ms (48.18%) | Memory: 44.3 MB (59.77%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -16,40 +16,35 @@ class Node {
         neighbors = _neighbors;
     }
 }
-
-
-1. Creat hashmap to store nodes
-    map { org_node: clone_node} //address
-2. if node is already present in map // means its created
-    return node address
-3. if its not present in map
-    creat a new node with org.val
-    Node(org.val)
-    make an entry into map
-4. repeat the same process for all the neighbours
-5. return the org nodes duplicate clone address
-
+1. Create a hashMap
+    to store all duplicate node address
+    {org: dup}   {2000: 9000}
+2. when ever we access a node,
+    if its already present in map
+    we dont create a new node again, we return its address
+3. start from 1st node && check if already extist in map
+    create a new node for it
+    return its address
+4. explore its neighbours
 */
 
 class Solution {
-    HashMap<Node,Node> map=new HashMap<>(); // {}
-
+    HashMap<Node,Node> map=new HashMap<>();
     public Node cloneGraph(Node node) {
         if(node==null){
             return node;
         }
 
-        if(map.containsKey(node)){
+        if(map.containsKey(node)==true){
             return map.get(node);
         }
-
-        Node clone=new Node(node.val);
-        map.put(node,clone);
-
+    
+        Node newnode=new Node(node.val);
+        map.put(node,newnode);
         for(Node neigh: node.neighbors){
-            clone.neighbors.add(cloneGraph(neigh));
+            newnode.neighbors.add(cloneGraph(neigh));
         }
 
-        return clone;
+        return newnode;
     }
 }
