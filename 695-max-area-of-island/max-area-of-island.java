5561fec75940d3d1e919e57a78a1ebd301b6c7bd
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-18 18:10:55 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (35.03%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,20 +1,24 @@
+
 class Solution {
-    private int dfs(int[][] grid,int row,int col){
-        if(row<0 || col<0 || row>=grid.length || col>=grid[row].length|| grid[row][col]==0){
+    public int dfs(int [][] grid,int row,int col){
+        if(row<0 || row>=grid.length || col<0 || col>=grid[0].length || grid[row][col]==0){
             return 0;
         }
         grid[row][col]=0;
-        return 1+dfs(grid,row-1,col)+dfs(grid,row+1,col)+dfs(grid,row,col-1)+dfs(grid,row,col+1); //right
+        return 1+dfs(grid,row,col+1)+dfs(grid,row,col-1)+ dfs(grid,row+1,col)+dfs(grid,row-1,col);//up
     }
-    public int maxAreaOfIsland(int[][] grid) {
-        int area=0;
-        for(int row=0;row<grid.length;row++){
-            for(int col=0;col<grid[row].length;col++){
-                if(grid[row][col]==1){
-                    area=Math.max(area,dfs(grid,row,col));
+     public int maxAreaOfIsland(int[][] grid){
+        int max=0;
+        int rows=grid.length;
+        int cols=grid[0].length;
+
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(grid[r][c]==1){
+                   max=Math.max(max,dfs(grid,r,c));
                 }
             }
         }
-        return area;
+        return max;
     }
 }
