c19e5877c1571d4066ddc9e077867b150bc8f14e
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-19 11:12:07 +0530

    Time: 7 ms (29.36%) | Memory: 48.3 MB (7.04%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,48 +1,50 @@
 class Solution {
     public int[] findOrder(int n, int[][] edges) {
-       ArrayList<Integer>[] adj=new ArrayList[n];
-        
-        for(int i=0;i<n;i++){
+        ArrayList<Integer>[] adj=new ArrayList[n];
+        for(int i=0;i<n;i++)
             adj[i]=new ArrayList<>();
-        }
         
         int[] indegree=new int[n];
         
-        for(int[] edge: edges){
-            int v1=edge[0];
-            int v2=edge[1];
+        for(int[] edge:edges){
+            int u=edge[0];
+            int v=edge[1];
             
-            adj[v2].add(v1); //directed
-            indegree[v1]++;
+            adj[v].add(u);
+            indegree[u]++;
         }
-        
         LinkedList<Integer> q=new LinkedList<>();
         
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.addLast(i);
+            }
+        }
+        ArrayList<Integer> res=new ArrayList<>();
         
-        for(int vertex=0;vertex<n;vertex++){
-           if(indegree[vertex]==0){
-                q.addLast(vertex);
-           }
+        while(!q.isEmpty()){
+            int vertex=q.removeFirst();
+            res.add(vertex);
+            
+            for(int neigh: adj[vertex]){
+                indegree[neigh]--;
+                
+                if(indegree[neigh]==0){
+                    q.addLast(neigh);
+                }
+            }
         }
         
-        int[] res=new int[n];
-       
-       int i=0;
-       while(!q.isEmpty()){
-           
-           int vertex=q.removeFirst();
-           res[i]=vertex;
-           
-           for(int neigh: adj[vertex]){
-               
-               indegree[neigh]--;
-               if(indegree[neigh]==0){
-                   q.addLast(neigh);
-               }
-           }
-           i++;
-       }
-      
-       return i==n ? res:new int[0];
+        System.out.println(res);
+        if(res.size()<n){
+            return new int[]{};
+        }
+
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=res.get(i);
+        }
+        return ans;
+        
     }
 }
