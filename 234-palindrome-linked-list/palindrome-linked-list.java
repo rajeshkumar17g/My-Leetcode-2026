0dff70d65c8962318a42c5edabfab6d4b043bdc2
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-22 15:04:33 +0530

    Time: 3 ms (99.83%) | Memory: 94.6 MB (44.39%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,37 +1,46 @@
 /*
-1. find middle of LL
-2. rev ll from head to middle
-3. compare 1st rev with second half (slow)
+1. FInd middle of LL
+2. Reverse 1st half
+3. compare 1st & 2nd half
+
+Note: Edge case of odd Nodes
+
 */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow=head,fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
+       ListNode slow=head,fast=head;
 
-        ListNode rev=null,after,crr=head;
-        while(crr!=slow){
+       while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+       }
+
+       ListNode crr=head,after,rev_head=null;
+
+       while(crr!=slow){
             after=crr.next;
-            crr.next=rev;
-            rev=crr;
+            crr.next=rev_head;
+            rev_head=crr;
             crr=after;
-        }
-
-        if(fast!=null){ // odd no of values , middle belongs to both the halfs , so skip it
-            slow=slow.next;
-        }
-
-        ListNode crr1=rev;
-        ListNode crr2=slow;
-        while(crr1!=null && crr2!=null){
+       }
+       if(fast!=null){ //odd number
+        slow=slow.next;// skipping middle value
+       }
+       
+       ListNode crr1=slow,crr2=rev_head;
+       
+       while(crr1!=null && crr2!=null){
             if(crr1.val!=crr2.val){
                 return false;
             }
             crr1=crr1.next;
             crr2=crr2.next;
-        }
-        return true;
+       }
+
+
+
+
+
+    return true;
     }
 }
