3c7038451f512d07fdf13bc7cde3f82260e654c5
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-23 14:47:47 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (79.00%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,27 +1,28 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        backtracking(nums,0,res);
-        return res;
-    }
-    private void swap(int[] nums,int i, int j){
+    public void swap(int[] nums,int i,int j){
         int temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
     }
-    private void backtracking(int[] nums,int i,  List<List<Integer>> res){
+
+    public void backtracking(int i,int[] nums, List<List<Integer>> res){
         if(i==nums.length){
-            List<Integer> copy=new ArrayList<>();
+            ArrayList<Integer> copy=new ArrayList<>();
             for(int ele: nums){
                 copy.add(ele);
             }
-            res.add(copy);
+            res.add(copy);// adding a copy to res
             return;
         }
         for(int j=i;j<nums.length;j++){
-            swap(nums,i,j);
-            backtracking(nums,i+1,res);
-            swap(nums,i,j);
+            swap(nums,i,j); //make the move
+            backtracking(i+1,nums,res); //backtracking to new level
+            swap(nums,i,j); // undo the move
         }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        backtracking(0,nums,res);
+        return res;
     }
 }
