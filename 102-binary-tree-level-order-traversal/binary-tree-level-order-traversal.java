bd47ab211b9c6a1627d3e7db66c2cd6fbc0a93c7
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-24 15:03:48 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (81.10%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,14 +1,25 @@
-class Solution {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res=new ArrayList<>();
-
         if(root==null){
             return res;
         }
-
          Queue<TreeNode> q=new LinkedList<>();
          q.offer(root);
-
          while(!q.isEmpty()){
             List<Integer> level=new ArrayList<>();
             int n=q.size();
@@ -25,8 +36,46 @@ class Solution {
             }
             res.add(level);
          }
-
-
          return res;
     }
 }
+ */
+class Solution {
+    public void dfs(  List<List<Integer>>  res,TreeNode root,int depth){
+        if(root==null){
+            return;
+        }
+        if(res.size()==depth){
+            res.add(new ArrayList<>());
+        }
+        res.get(depth).add(root.val);
+        dfs(res,root.left,depth+1);
+        dfs(res,root.right,depth+1);
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>>  res=new ArrayList<>();
+        dfs(res,root,0);
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
