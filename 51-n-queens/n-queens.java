1db89af71270f1f432f5d03b5d2c5e3a14624f0b
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-24 11:07:05 +0530

    Time: 5 ms (22.79%) | Memory: 46.7 MB (28.87%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,56 +1,49 @@
 class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        ArrayList<Integer> board=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            board.add(-1);
-        }
-        List<List<String>> res=new ArrayList<>();
-        backtracking(0,n,board,res); // row=0 board={-1,-1,-1,-1} //res=[]
-        return res;
-    }
-    private boolean is_safe(ArrayList<Integer> board,int row,int col){
-
-        for(int i=0;i<row;i++){
-            if(board.get(i)==col || (Math.abs(i-row)==Math.abs(board.get(i)-col))){
+    public boolean is_valid(int[] board,int x2,int y2){
+        for(int x1=0;x1<x2;x1++){
+            int y1=board[x1];
+            if(y1==y2 || Math.abs(x1-x2)==Math.abs(y1-y2)){
                 return false;
             }
         }
         return true;
     }
-    private void backtracking(int row,int n,ArrayList<Integer> board,List<List<String>> res){
-        if(row==n){
-            ArrayList<String> sol=new ArrayList<>();
-           
+    public void backtracking(int n, int[] board,List<List<String>> res,int row){
+        if(row==n){ //board[1,3,0,2];
+            ArrayList<String> copy=new ArrayList<>();
             for(int i=0;i<n;i++){
-                 String r="";
+                String row_list="";
                 for(int j=0;j<n;j++){
-                    if(j==board.get(i)){
-                        r=r+"Q";
+                    if(board[i]==j){
+                       row_list+="Q";
                     }
                     else{
-                        r=r+".";
+                       row_list+=".";
                     }
                 }
-                sol.add(r);
+                copy.add(row_list);
             }
-
-            res.add(new ArrayList(sol));//adding a dup copy of res
+            res.add(copy);
             return;
         }
+
         for(int col=0;col<n;col++){
-            if(is_safe(board,row,col)){
-                board.set(row,col);
-                backtracking(row+1,n,board,res);
-                board.set(row,-1);
+            if(is_valid(board,row,col)==true){
+                board[row]=col; //make the move
+                backtracking(n,board,res,row+1);// may lead to a solution
+                board[row]=-1;  //undo the move
             }
         }
+    }
 
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res= new ArrayList<>();
+        int[] board=new int[n];
+        Arrays.fill(board,-1);
 
+        backtracking(n,board,res,0);
 
-
-
-
-
+        return res;
 
     }
 }
