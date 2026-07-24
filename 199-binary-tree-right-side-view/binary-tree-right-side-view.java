8c92c63dd0b43a1ed6db9486f57a42b128d730e9
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-24 15:48:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (69.56%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,25 +13,21 @@
  *     }
  * }
  */
+
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        
-    List<Integer> res=new ArrayList<>();
-     traversal(root,res,0);
-     return res;
-    }
-    private void traversal(TreeNode root, List<Integer> res,int depth){
+    public void dfs(List<Integer>  res,TreeNode root,int depth){
         if(root==null){
             return;
-
         }
         if(res.size()==depth){
-            res.add(root.val);
+           res.add(root.val);
         }
-      
-        
-        traversal(root.right,res,depth+1);
-        traversal(root.left,res,depth+1);
-
+        dfs(res,root.right,depth+1);
+        dfs(res,root.left,depth+1);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer>  res=new ArrayList<>();
+        dfs(res,root,0);
+        return res;
     }
 }
