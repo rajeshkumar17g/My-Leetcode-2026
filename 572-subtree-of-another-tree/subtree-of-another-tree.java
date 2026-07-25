009442d1eb86f2ab483e2640252304032b744c06
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-25 10:26:44 +0530

    Time: 3 ms (64.98%) | Memory: 46.4 MB (70.80%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -14,21 +14,20 @@
  * }
  */
 class Solution {
-     public boolean isSameTree(TreeNode p, TreeNode q) {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null){
             return true;
         }
-        else if(p==null ||q==null || p.val!=q.val){
+        if(p==null || q==null || p.val!=q.val){
             return false;
         }
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(root==null){
             return false;
         }
-        if(isSameTree(root,subRoot)){
+        if(isSameTree(root,subRoot)==true){
             return true;
         }
         return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
