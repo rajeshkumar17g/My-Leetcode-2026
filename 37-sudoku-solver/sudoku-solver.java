8f1c4d84e8385bbe0a67112f518878539e75b3d2
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-27 13:30:09 +0530

    Time: 268 ms (46.61%) | Memory: 43 MB (18.36%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,59 +1,47 @@
 class Solution {
-        public boolean is_valid(char[][] board,int row,int col,char ch){
-        
-        int num=ch;
-        // rows
+        public boolean is_valid(char[][] board,int row,int col,char choice){
+        char num=choice;
+        // same row or col
         for(int i=0;i<9;i++){
-            if(board[row][i]==num && i!=col){
+            if(board[row][i]==num && col!=i || board[i][col]==num && row!=i){
                 return false;
             }
         }
-         // cols
-        for(int i=0;i<9;i++){
-            if(board[i][col]==num && i!=row){
-                return false;
-            }
-        }
-
         int r=(row/3)*3;
         int c=(col/3)*3;
-
         for(int i=r;i<r+3;i++){
             for(int j=c;j<c+3;j++){
-                if(board[i][j]==num && (i!=row && j!=col)){
+                if(num==board[i][j] && (i!=row && j!=col)){
                     return false;
                 }
             }
         }
 
         return true;
-
-
     }
-    public boolean backtrack(char[][] board){
-           for(int row=0;row<9;row++){
+    public boolean backtracking(char[][] board){
+         for(int row=0;row<9;row++){
             for(int col=0;col<9;col++){
-                if(board[row][col]=='.') // empty cell
-                {
-                    // for choice in choices
-                    for(char ch='1';ch<='9';ch++){
-                        if(is_valid(board,row,col,ch)==true){
-                            board[row][col]=ch; //make the move  fill the cell
-                            if(backtrack(board)==true){
+                if(board[row][col]=='.'){
+                    for(char choice='1';choice<='9';choice++){
+                        if(is_valid(board,row,col,choice)==true){
+                            board[row][col]=choice; //making a choice
+                            if(backtracking(board)==true){
                                 return true;
-                            }  // may lead to a solution
-                            board[row][col]='.'; // undo the move && try to fill another number
+                            }   // explore that path --> may lead to a solution
+                            board[row][col]='.';  //undo the choice
+
                         }
                     }
                     return false;
+
                 }
             }
         }
-        return true; // if all 0-8 rows are filled 
-
+        return true;
     }
     public void solveSudoku(char[][] board) {
-     
-        backtrack(board);
+        backtracking(board);
     }
+
 }
