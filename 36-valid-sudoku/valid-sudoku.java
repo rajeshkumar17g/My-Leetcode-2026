6626cb53e14526756cb36343809330fed6d49b9f
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-27 12:09:23 +0530

    Time: 1 ms (99.93%) | Memory: 46.2 MB (93.39%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,45 +1,43 @@
 class Solution {
     public boolean is_valid(char[][] board,int row,int col){
-        
-        int num=board[row][col];
-        // rows
+        char num=board[row][col];
+        // same row
         for(int i=0;i<9;i++){
-            if(board[row][i]==num && i!=col){
+            if(board[row][i]==num && col!=i){
                 return false;
             }
         }
-         // cols
+        // same col
         for(int i=0;i<9;i++){
-            if(board[i][col]==num && i!=row){
+            if(board[i][col]==num && row!=i){
                 return false;
             }
         }
 
         int r=(row/3)*3;
         int c=(col/3)*3;
-
         for(int i=r;i<r+3;i++){
             for(int j=c;j<c+3;j++){
-                if(board[i][j]==num && (i!=row && j!=col)){
+                if(num==board[i][j] && (i!=row && j!=col)){
                     return false;
                 }
             }
         }
 
         return true;
-
-
     }
     public boolean isValidSudoku(char[][] board) {
-       for(int row=0;row<9;row++){
-        for(int col=0;col<9;col++){
-            if(board[row][col]!='.'){
-                if(is_valid(board,row,col)==false){
-                    return false;
+        for(int row=0;row<9;row++){
+            for(int col=0;col<9;col++){
+                if(board[row][col]!='.'){
+                    if(is_valid(board,row,col)==false){
+                        return false;
+                    }
                 }
             }
         }
-       }
-       return true;
+        return true;
+
+
     }
 }
