d72884a9431875f6a16b8fbeff9454876c68cfa1
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-27 10:22:33 +0530

    Time: 1 ms (86.73%) | Memory: 43.8 MB (95.17%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,42 +1,37 @@
 class Solution {
-     public void backtracking(int i, List<Integer> subset,int[] nums,List<List<Integer>> res){
-            res.add(new ArrayList<>(subset));
-            for(int j=i;j<nums.length;j++){
-                subset.add(nums[j]);
-                backtracking(j+1,subset,nums,res);
-                subset.remove(subset.size()-1);
-            }
-     }
+    public void backtracking(int[] nums,List<List<Integer>> res, List<Integer> subset,int i){
+        res.add(new ArrayList<>(subset));
+        for(int j=i;j<nums.length;j++){
+            subset.add(nums[j]);
+            backtracking(nums,res,subset,j+1);
+            subset.remove(subset.size()-1);
+        }
+    }
     public List<List<Integer>> subsets(int[] nums) {
-           List<Integer> subset=new ArrayList<>();
-        List<List<Integer>> res= new ArrayList<>();
-
-        backtracking(0,subset,nums,res);
-
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> subset=new ArrayList<>();
+        backtracking(nums,res,subset,0); // subset=[]  i=0
         return res;
     }
 }
-
 /*
+
 class Solution {
-    public void backtracking(int i, List<Integer> subset,int[] nums,List<List<Integer>> res){
+    public void backtracking(int[] nums,List<List<Integer>> res, List<Integer> subset,int i){
         if(i==nums.length){
             res.add(new ArrayList<>(subset));
             return;
         }
-        subset.add(nums[i]); 
-        backtracking(i+1,subset,nums,res);
-        subset.remove(subset.size()-1); 
-        backtracking(i+1,subset,nums,res);
+        subset.add(nums[i]);
+        backtracking(nums,res,subset,i+1);//[] -> [a] -> [a,b] -> [a,b]
+        subset.remove(subset.size()-1);
+        backtracking(nums,res,subset,i+1);
     }
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
         List<Integer> subset=new ArrayList<>();
-        List<List<Integer>> res= new ArrayList<>();
-
-        backtracking(0,subset,nums,res);
-
+        backtracking(nums,res,subset,0); // subset=[]  i=0
         return res;
-
     }
 }
 */
