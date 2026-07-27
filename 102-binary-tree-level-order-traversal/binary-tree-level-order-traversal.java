d7418445775dd76a7884476ff98448680da59fbf
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-27 13:56:51 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (81.25%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -12,70 +12,22 @@
  *         this.right = right;
  *     }
  * }
- class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res=new ArrayList<>();
-        if(root==null){
-            return res;
-        }
-         Queue<TreeNode> q=new LinkedList<>();
-         q.offer(root);
-         while(!q.isEmpty()){
-            List<Integer> level=new ArrayList<>();
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                TreeNode node=q.poll();
-                level.add(node.val);
-
-                if(node.left!=null){
-                    q.offer(node.left);
-                }
-                if(node.right!=null){
-                    q.offer(node.right);
-                }
-            }
-            res.add(level);
-         }
-         return res;
-    }
-}
  */
 class Solution {
-    public void dfs(  List<List<Integer>>  res,TreeNode root,int depth){
+    public void dfs(TreeNode root, List<List<Integer>> res,int depth){
         if(root==null){
             return;
         }
-        if(res.size()==depth){
+        if(depth==res.size()){
             res.add(new ArrayList<>());
         }
         res.get(depth).add(root.val);
-        dfs(res,root.left,depth+1);
-        dfs(res,root.right,depth+1);
+        dfs(root.left,res,depth+1);
+        dfs(root.right,res,depth+1);
     }
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>>  res=new ArrayList<>();
-        dfs(res,root,0);
+        List<List<Integer>> res=new ArrayList<>();
+        dfs(root,res,0);
         return res;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
