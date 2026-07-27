9c57e87bbee580f863a6de10e7589040dae65b0a
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-27 11:45:17 +0530

    Time: 26 ms (9.85%) | Memory: 98 MB (46.45%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,25 +1,18 @@
 class Solution {
-    private void backtracking(int i,int n,int k,List<Integer> subset, List<List<Integer>> res){
-        if(subset.size()>k){
-            return;
-        }
-        if(k==subset.size()){
-            res.add(new ArrayList<>(subset));
+    public void backtracking(int n,List<List<Integer>> res, List<Integer> subset,int i,int k){
+        if(subset.size()==k){
+                res.add(new ArrayList<>(subset));
         }
         for(int j=i;j<=n;j++){
             subset.add(j);
-            backtracking(j+1,n,k,subset,res);
+            backtracking(n,res,subset,j+1,k);
             subset.remove(subset.size()-1);
         }
-
     }
-    public List<List<Integer>> combine(int n, int k) {
-
+    public List<List<Integer>> combine(int n, int k)  {
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> subset=new ArrayList<>();
-
-        backtracking(1,n,k,subset,res);
-
+        backtracking(n,res,subset,1,k); // subset=[]  i=0
         return res;
     }
 }
