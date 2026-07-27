f47e56c06ea8c4a43d315e85d3e283b2f69833c6
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-27 10:58:00 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (28.92%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -4,25 +4,24 @@ class Solution {
         nums[i]=nums[j];
         nums[j]=temp;
     }
-
-    public void backtracking(int i,int[] nums, List<List<Integer>> res){
+    public void backtracking(int[] nums,List<List<Integer>> res,int i){
         if(i==nums.length){
-            ArrayList<Integer> copy=new ArrayList<>();
+            List<Integer> copy=new ArrayList<>();
             for(int ele: nums){
                 copy.add(ele);
             }
-            res.add(copy);// adding a copy to res
+            res.add(copy);
             return;
         }
         for(int j=i;j<nums.length;j++){
-            swap(nums,i,j); //make the move
-            backtracking(i+1,nums,res); //backtracking to new level
-            swap(nums,i,j); // undo the move
+            swap(nums,i,j); // make the choice
+            backtracking(nums,res,i+1); //explore that path
+            swap(nums,i,j); // undo the choice
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
-        backtracking(0,nums,res);
+        backtracking(nums,res,0);
         return res;
     }
 }
