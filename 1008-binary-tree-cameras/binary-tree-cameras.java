888fdc8a69a2c1118549fe7f26f2b559153683cf
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-29 10:51:21 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (12.54%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.java
+++ 1008-binary-tree-cameras/binary-tree-cameras.java
@@ -1,32 +1,28 @@
 
 class Solution {
     int count=0;
-    public String traversal(TreeNode root){
+    public String dfs(TreeNode root){
         if(root==null){
             return "";
         }
-
         if(root.left==null && root.right==null){
             return "leaf";
         }
+        String left=dfs(root.left);
+        String right=dfs(root.right);
 
-        String left=traversal(root.left);
-        String right=traversal(root.right);
-
-        if(left.equals("leaf")|| right.equals("leaf")){
+        if(left.equals("leaf") || right.equals("leaf")){
             count++;
             return "camera";
-        }  
-        else if(left.equals("camera")|| right.equals("camera")){
-            
+        }
+        if(left.equals("camera") || right.equals("camera")){
             return "gap";
-        }  
+        }
         return "leaf";
 
     }
     public int minCameraCover(TreeNode root) {
-      
-        if(traversal(root).equals("leaf")==true){
+        if(dfs(root).equals("leaf")){
             count++;
         }
         return count;
