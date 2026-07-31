631a3ad500bcf33bdbeab806b0c04fa93cc84c8b
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-07-31 09:51:03 +0530

    Time: 521 ms (32.51%) | Memory: 76.6 MB (65.45%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,36 +1,36 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        
         int n=points.length;
-
         PriorityQueue<int []> q=new PriorityQueue<>((a,b)->a[1]-b[1]);
+        q.offer(new int[]{0,0});
         boolean[] visited=new boolean[n];
-        
-        q.offer(new int[] {0,0});   //starting from 0  weight costed =0
-       
         int min_cost=0;
-        
+
         while(!q.isEmpty()){
-            
-            int[] edge=q.poll();
-            
-            int vertex=edge[0];
-            int cost=edge[1];
-            //System.out.println(vertex+" "+weight);
+            int[] node=q.poll();
+            int vertex=node[0];
+            int cost=node[1];
+
             if(visited[vertex]==true){
                 continue;
             }
-            
-            min_cost+=cost;
             visited[vertex]=true;
-            for(int neigh=0;neigh<n;neigh++){
-                
-                if(visited[neigh]==false){
-                    int distance=Math.abs(points[neigh][0]-points[vertex][0])+Math.abs(points[neigh][1]-points[vertex][1]);
-                    q.offer(new int[]{neigh,distance});
+            min_cost+=cost;
+
+            for(int i=0;i<n;i++){
+                if(visited[i]==false){
+                    int x1=points[vertex][0];
+                    int y1=points[vertex][1];
+
+                    int x2=points[i][0];
+                    int y2=points[i][1];
+
+                    int dist=Math.abs(x1-x2)+Math.abs(y1-y2);
+                    q.offer(new int[]{i,dist});
                 }
             }
+
         }
-          return min_cost; 
+        return min_cost;
     }
 }
