58f263742401b3742a349777ebc81ce6cd06fb1d
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-04 09:59:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.52%) - LeetSync

---
+++ 69-sqrtx/sqrtx.c
@@ -0,0 +1,17 @@
+int mySqrt(int x) {
+    int low=0,high=x;
+    while(low<=high){
+        long int mid=(low+(high-low)/2);
+        if((mid*mid)==x){
+            return mid;
+        }
+        else if(mid*mid>x){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return high;
+
+}
