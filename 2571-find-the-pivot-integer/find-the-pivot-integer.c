9a356fac12b217d5dbaa94ec9fa03249529c2439
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-06 12:28:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (83.12%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.c
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.c
@@ -1,5 +1,17 @@
 int pivotInteger(int n) {
-    int low=1,high=n;
+    int total=n*(n+1)/2;
+    int prefix_sum=0;
+   for(int i=1;i<=n;i++){
+        prefix_sum=prefix_sum+i;
+        if(prefix_sum==(total-prefix_sum+i)){
+            return i;
+        }
+   }
+   return -1;
+}
+
+/*
+ int low=1,high=n;
     int sum=n*(n+1)/2;
     while(low<=high){
         int mid=low+(high-low)/2;
@@ -17,4 +29,4 @@ int pivotInteger(int n) {
         }
     }
     return -1;
-}
+    */
