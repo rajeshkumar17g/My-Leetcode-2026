907f3e8114cdd651e8d813b37ec9bcf1e42a64da
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-11 15:34:05 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (30.30%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,26 +1,33 @@
 class Solution:
-    '''
-    10 10 20 30 30 40 40  50 50 60 60 
-     0  1  2  3  4  5  6  7  8  9  10 
-     l     m     h  m               
-
-    '''
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        low=0
-        high=len(nums)-1
-        while(low<high):
-            mid=(low+high)//2
-            print(low,high,mid)
-            if(nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1]):
+        if len(nums) == 1:
+            return nums[0]
+
+        if nums[0] != nums[1]:
+            return nums[0]
+
+        if nums[-1] != nums[-2]:
+            return nums[-1]
+
+        low = 1
+        high = len(nums) - 2
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] != nums[mid + 1] and nums[mid] != nums[mid - 1]:
                 return nums[mid]
-            elif(nums[mid]==nums[mid-1]):
-                if((mid-1)%2==0):
-                    low=mid+2
+
+            elif nums[mid] == nums[mid - 1]:
+                if (mid - 1) % 2 == 0:
+                    low = mid + 1
                 else:
-                    high=mid-2
+                    high = mid - 2
+
             else:
-                if((mid)%2==0):
-                    low=mid+2
+                if mid % 2 == 0:
+                    low = mid + 2
                 else:
-                    high=mid-1
-        return nums[high]
+                    high = mid - 1
+
+        return -1
