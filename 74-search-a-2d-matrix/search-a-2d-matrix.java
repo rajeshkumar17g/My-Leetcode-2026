051154cc38442c2834dd18f613622e8deee3476f
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-12 11:19:08 +0530

    Time: 2 ms (1.49%) | Memory: 43.7 MB (80.31%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,21 +1,29 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int low=0,high=m-1,row=0;
-        while(low<high){
-            int mid=low+(high-low+1)/2;
-            if(target>=matrix[mid][0]){
-                low=mid;
+        
+        int row=0;
+        int m=matrix.length,n=matrix[0].length;
+
+        int low=0,high=m-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target>=matrix[mid][0] && target<=matrix[mid][n-1]){
+                row=mid;
+                break;
+            }
+            else if(target>matrix[mid][n-1]){
+                low=mid+1;
             }
             else{
                 high=mid-1;
             }
         }
-        row=low;
-        low=0;high=n-1;
+        System.out.println(row);
+        low=0;
+        high=n-1;
         while(low<=high){
-            int mid=low+(high-low)/2;
+            int mid=(low+high)/2;
+
             if(target==matrix[row][mid]){
                 return true;
             }
@@ -26,8 +34,8 @@ class Solution {
                 low=mid+1;
             }
         }
-
         return false;
+        
 
     }
 }
