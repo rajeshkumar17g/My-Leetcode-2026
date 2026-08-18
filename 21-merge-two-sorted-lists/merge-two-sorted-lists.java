3dc0f1bb235d52154279324aa4021edb05df092a
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-18 11:07:33 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (88.95%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,7 +1,6 @@
 
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-
         ListNode dummy=new ListNode();
         ListNode prev=dummy;
 
@@ -9,23 +8,22 @@ class Solution {
             if(l1.val<=l2.val){
                 prev.next=l1;
                 l1=l1.next;
+                prev=prev.next;
             }
             else{
                 prev.next=l2;
                 l2=l2.next;
+                prev=prev.next;
             }
-            prev=prev.next;
         }
+
         if(l1!=null){
             prev.next=l1;
         }
-        else if(l2!=null){
+        else{
             prev.next=l2;
         }
+
         return dummy.next;
-
-
-
-
     }
 }
