91ed6b3e1110ce3e2ae8c5bf933cb6898460b6b7
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-18 10:09:22 +0530

    Time: 3 ms (99.83%) | Memory: 94.8 MB (34.79%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,46 +1,50 @@
-/*
-1. FInd middle of LL
-2. Reverse 1st half
-3. compare 1st & 2nd half
-
-Note: Edge case of odd Nodes
-
-*/
 class Solution {
     public boolean isPalindrome(ListNode head) {
-       ListNode slow=head,fast=head;
+        
+        ListNode l1,l2;
+        ListNode slow=head,fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
 
-       while(fast!=null && fast.next!=null){
-        slow=slow.next;
-        fast=fast.next.next;
-       }
-
-       ListNode crr=head,after,rev_head=null;
-
-       while(crr!=slow){
+        
+        ListNode crr=head,after,rev_head=null;
+        while(crr!=slow){
             after=crr.next;
             crr.next=rev_head;
             rev_head=crr;
             crr=after;
-       }
-       if(fast!=null){ //odd number
-        slow=slow.next;// skipping middle value
-       }
-       
-       ListNode crr1=slow,crr2=rev_head;
-       
-       while(crr1!=null && crr2!=null){
-            if(crr1.val!=crr2.val){
+        }
+
+        l1=rev_head;
+
+        //even or odd
+        if(fast!=null){
+            //odd
+            l2=slow.next;
+        }
+        else{
+            l2=slow;
+        }
+
+        while(l1!=null && l2!=null){
+            if(l1.val!=l2.val){
                 return false;
             }
-            crr1=crr1.next;
-            crr2=crr2.next;
-       }
+            l1=l1.next;
+            l2=l2.next;
+        }
+        return true;
 
-
-
-
-
-    return true;
     }
 }
+/*
+1. find middle of LL
+2. determine even or odd number of elements
+3. reverse the first half <slow
+4. depending on even or odd, select your starting point of
+5. compare
+
+
+*/
