7fdac1151cde1682c84fb856285dcc2e6bc15a76
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-21 15:57:17 +0530

    Time: 3 ms (92.18%) | Memory: 47.2 MB (9.40%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -4,29 +4,31 @@ class Solution {
         nums[i]=nums[j];
         nums[j]=temp;
     }
-    public void backtracking(int[] nums,List<List<Integer>> res,int i){
+    public void backtracking(List<List<Integer>> res,int[] nums,int i){
         if(i==nums.length){
-            List<Integer> copy=new ArrayList<>();
+            List<Integer> ans=new ArrayList<>();
             for(int ele: nums){
-                copy.add(ele);
+                ans.add(ele);
             }
-            res.add(copy);
+            res.add(ans);
             return;
         }
-        HashSet<Integer> seen=new HashSet<>();
-        for(int j=i;j<nums.length;j++){
-            if(seen.contains(nums[j])==true){
+
+        HashSet<Integer> set=new HashSet<>();     
+        for(int j=i;j<nums.length;j++){ // i=0 j=0,1,2 // i=1  j=1,2 //i=2 j=2
+            if(set.contains(nums[j])==true){
                 continue;
             }
-            seen.add(nums[j]);
-            swap(nums,i,j); // make the choice
-            backtracking(nums,res,i+1); //explore that path
-            swap(nums,i,j); // undo the choice
+            set.add(nums[j]);
+            swap(nums,i,j);  //make the choice
+            backtracking(res,nums,i+1); //backtrack
+            swap(nums,i,j);  // undo the move
         }
     }
+
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
-        backtracking(nums,res,0);
+        backtracking(res,nums,0);
         return res;
     }
 }
