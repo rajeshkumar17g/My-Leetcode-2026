e0dddcefaf6389b5c6ae38722aabcdec9e6de2ce
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-25 15:01:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (88.32%) - LeetSync

--- 507-perfect-number/perfect-number.c
+++ 507-perfect-number/perfect-number.c
@@ -1,8 +1,11 @@
 bool checkPerfectNumber(int num) {
-    int sum=0;
-    for(int i=1;i<=num/2;i++){
+    if(num==1){
+        return false;
+    }
+    int sum=1;
+    for(int i=2;i<=(int)sqrt(num);i++){
         if(num%i==0){
-            sum=sum+i;
+            sum=sum+i+num/i;
         }
     }
     return sum==num;
