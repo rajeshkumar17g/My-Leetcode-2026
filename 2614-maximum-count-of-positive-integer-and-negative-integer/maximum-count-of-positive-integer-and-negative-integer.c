ef5e0dd415ad665a75f3b32548603950489a86ee
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-27 14:01:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (45.41%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.c
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.c
@@ -1,6 +1,50 @@
 int maximumCount(int* nums, int numsSize) {
     
-    int pcount=0,ncount=0;
+    int ng=-1;
+    int low=0,high=numsSize-1;
+
+    while(low<=high){
+        int mid=low+(high-low)/2;
+
+        if(nums[mid]<0){
+            ng=mid;
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    int ncount=ng+1;
+
+
+ int po=-1;
+    low=0,high=numsSize-1;
+
+    while(low<=high){
+        int mid=low+(high-low)/2;
+
+        if(nums[mid]>0){
+            po=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+
+    int pcount;
+    if(po!=-1)
+        pcount=numsSize-po;
+    else
+        pcount=0;
+
+    return (pcount>ncount)?pcount:ncount;
+}
+
+
+/*
+
+int pcount=0,ncount=0;
     for(int index=0;index<numsSize;index++){
         if(nums[index]>0){
             pcount++;
@@ -11,4 +55,4 @@ int maximumCount(int* nums, int numsSize) {
     }
 
     return (pcount>ncount)?pcount:ncount;
-}
+*/
