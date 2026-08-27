61e0047ad6f59b301eb378221c68abca2409b458
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-27 10:10:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (93.85%) - LeetSync

--- 35-search-insert-position/search-insert-position.c
+++ 35-search-insert-position/search-insert-position.c
@@ -1,19 +1,20 @@
 int searchInsert(int* nums, int numsSize, int target) {
-        int low=0,high=numsSize-1;
+     int low=0,high=numsSize-1;
 
-    while(low<=high){
-        int mid=low+(high-low)/2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
 
-        if(target==nums[mid]){
-            return mid;
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(target<nums[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
-        else if(target>nums[mid]){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
-    }
 
-    return low;
+        return low;
+
 }
