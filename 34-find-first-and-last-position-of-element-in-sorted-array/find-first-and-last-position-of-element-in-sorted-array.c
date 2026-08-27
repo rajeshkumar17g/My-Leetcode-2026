6b82b5c3866576f57edd6417fc4154ae1064e8d5
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-27 12:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (94.19%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.c
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.c
@@ -1,48 +1,50 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
 int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
-    // first occurance
-    int low=0,high=numsSize-1;
-    int fo=-1;
-    while(low<=high){
-        int mid=low+(high-low)/2;
 
-        if(target==nums[mid]){
-           fo=mid;
-           high=mid-1;
-        }
-        else if(target>nums[mid]){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
-    }
+            *returnSize=2;
+            // first occurance
+            int low=0,high=numsSize-1;
+            static int res[2]; // res[0]=first occ   res[1]=l;ast
 
-    // first occurance
-    low=0,high=numsSize-1;
-    int lo=-1;
-    while(low<=high){
-        int mid=low+(high-low)/2;
+            res[0]=-1;
+            while(low<=high){
+                int mid=low+(high-low)/2;
 
-        if(target==nums[mid]){
-           lo=mid;
-           low=mid+1;
-        }
-        else if(target>nums[mid]){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
-    }
+                if(target==nums[mid]){
+                    res[0]=mid;
+                    high=mid-1;
+                }
+                else if(target>nums[mid]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
 
-    static int arr[2];
-    arr[0]=fo;
-    arr[1]=lo;
-    *returnSize=2;
-    return arr;
+
+
+
+              // last occurance
+            low=0,high=numsSize-1;
+           
+
+            res[1]=-1;
+            while(low<=high){
+                int mid=low+(high-low)/2;
+
+                if(target==nums[mid]){
+                    res[1]=mid;
+                    low=mid+1;
+                }
+                else if(target>nums[mid]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+
+            return res;
 
 
 }
