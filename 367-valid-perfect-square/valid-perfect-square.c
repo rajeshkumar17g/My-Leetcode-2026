b639c4981c5c4cce7769d037a8a3836efe90c5b9
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-27 10:38:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (16.79%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.c
+++ 367-valid-perfect-square/valid-perfect-square.c
@@ -1,17 +1,19 @@
-
 bool isPerfectSquare(int x) {
-    int low=1,high=x;
-    while(low<=high){
-        long int mid=(low+(high-low)/2);
-        if((mid*mid)==x){
-            return true;
+    int low=0,high=x;
+
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            
+            if(mid*mid==x){
+                return true;
+            }
+            else if(mid*mid<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
-        else if(mid*mid>x){
-            high=mid-1;
-        }
-        else{
-            low=mid+1;
-        }
-    }
-    return false;
+
+        return false;
 }
