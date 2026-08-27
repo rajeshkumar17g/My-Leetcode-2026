ec777b7920837eb49bd25a7ee99368cb26b0cfa0
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-27 14:56:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (46.74%) - LeetSync

--- 344-reverse-string/reverse-string.c
+++ 344-reverse-string/reverse-string.c
@@ -1,6 +1,8 @@
 void reverseString(char* s, int sSize) {
+    
     int left=0,right=sSize-1;
-    while(left<=right){
+
+    while(left<right){
         char temp=s[left];
         s[left]=s[right];
         s[right]=temp;
