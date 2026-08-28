bec75c62e1831c971a99cc4a2bef60358aecc36a
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-28 10:36:52 +0530

    Time: 7 ms (11.37%) | Memory: 195 MB (19.37%) - LeetSync

--- 189-rotate-array/rotate-array.c
+++ 189-rotate-array/rotate-array.c
@@ -1,32 +1,30 @@
-void reverse(int* arr, int left,int right){
+void reverse(int* nums,int left,int right){
 
-    while(left<=right){
-        int temp=arr[left];
-        arr[left]=arr[right];
-        arr[right]=temp;
+    while(left<right){
+        int temp=nums[left];
+        nums[left]=nums[right];
+        nums[right]=temp;
         left++;
         right--;
     }
-
 }
-void rotate(int* arr, int n, int k) {
-    
-    k=k%n; // maximum length number of rotations we have to do
-    reverse(arr,0,n-1);
-    reverse(arr,0,k-1); //
-    reverse(arr,k,n-1);
+void rotate(int* nums, int n, int k) {
+    k=k%n;
+    reverse(nums,0,n-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,n-1);
 
 }
 
 /*
+ k=k%numsSize;
+    for(int j=1;j<=k;j++){
 
-    for(int t=1;t<=k;t++){
-
-        int last=arr[n-1];
-        for(int i=n-1;i>=1;i--){
-            arr[i]=arr[i-1];
+        int last=nums[numsSize-1];
+        for(int i=numsSize-1;i>=1;i--){
+            nums[i]=nums[i-1];
         }
-        arr[0]=last;
-        
+        nums[0]=last;
     }
+
 */
