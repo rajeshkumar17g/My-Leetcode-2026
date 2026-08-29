5559ffe25309602e33f4c54abcc39aa698058c7d
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-29 14:28:57 +0530

    Time: 9 ms (78.77%) | Memory: 11.9 MB (47.68%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.c
+++ 141-linked-list-cycle/linked-list-cycle.c
@@ -1,5 +1,12 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 bool hasCycle(struct ListNode *head) {
+    
     struct ListNode *slow=head,*fast=head;
     while(fast!=NULL && fast->next!=NULL){
         slow=slow->next;
