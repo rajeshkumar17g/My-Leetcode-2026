4d157b7be3d68b5138c008515b3fa874faf193f1
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-08-31 10:10:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (82.08%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.c
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.c
@@ -1,15 +1,28 @@
-struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
-    struct ListNode *fast=head,*slow=head;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
 
-    int count=0;
-    while(count<n){
+ 1. move fast pointer n steps
+ 2. slow=head
+ 3. move both slow & fast pointer 1 step => fast.next!=null
+ 4. slow.next=slow.next.next
+ */
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    
+    struct ListNode *fast=head;
+    int count=1;
+
+    while(count<=n){
         fast=fast->next;
         count++;
     }
     if(fast==NULL){
         return head->next;
     }
-
+    struct ListNode *slow=head;
     while(fast->next!=NULL){
         slow=slow->next;
         fast=fast->next;
@@ -19,3 +32,19 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
 
     return head;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
