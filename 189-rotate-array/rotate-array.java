932dca75a248e3d1bf58c36f095a43c813d1d505
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-03 12:07:17 +0530

    Time: 4 ms (30.58%) | Memory: 268.7 MB (16.36%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,31 +1,40 @@
 class Solution {
-    public static void reverse(int[] nums,int left,int right){
-            while(left<=right){
-                int temp=nums[left];
-                nums[left]=nums[right];
-                nums[right]=temp;
-                left++;
-                right--;
-            }
+    public void reverse(int[] nums,int left,int right){
+
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+
+            left++;
+            right--;
         }
+
+    }
     public void rotate(int[] nums, int k) {
-        
+
         int n=nums.length;
         k=k%n;
-        reverse(nums,0,n-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,n-1);
-     
+
+        reverse(nums,0,n-1); 
+        reverse(nums,0,k-1); 
+        reverse(nums,k,n-1); 
     }
 }
 
-/*
 
-k=k%nums.length;
-     for(int i=1;i<=k;i++){
-        int last=nums[nums.length-1];
-        for(int index=nums.length-1;index>0;index--){
-            nums[index]=nums[index-1];
-        }   
-        nums[0]=last;
-     }*/
+/*
+int n=nums.length;
+        k=k%n; //maximum length no of rotations
+
+        for(int j=1;j<=k;j++){
+
+            int last=nums[n-1];
+            for(int i=n-1;i>=1;i--){
+                nums[i]=nums[i-1];
+            }
+            nums[0]=last;
+
+        }
+
+*/
