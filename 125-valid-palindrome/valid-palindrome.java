155fc235ec6a270c436e2866fa595316edd14eb7
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-05 09:46:17 +0530

    Time: 2 ms (99.31%) | Memory: 44.4 MB (53.87%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,6 +1,41 @@
 class Solution {
+    public boolean isalnum(char ch){
+        if(ch>='a' && ch<='z' || ch>='A' && ch<='Z' || ch>='0' && ch<='9'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     public boolean isPalindrome(String s) {
         
+
+        int left=0,right=s.length()-1;
+        while(left<right){
+
+            while(left<right && isalnum(s.charAt(left))==false){
+                left++;
+            }
+
+            while(left<right && isalnum(s.charAt(right))==false){
+                right--;
+            }
+
+            if(left<right && Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+
+    }
+}
+
+
+/*
+
         String lc="";
         for(int i=0;i<s.length();i++){
 
@@ -26,7 +61,4 @@ class Solution {
        }
        return true;
 
-
-
-    }
-}
+*/
