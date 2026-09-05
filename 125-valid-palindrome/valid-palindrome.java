ecf8853c89f6b0f65c6cbf46deb3854d1777146e
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-05 09:29:27 +0530

    Time: 129 ms (5.18%) | Memory: 48.2 MB (5.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,61 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-
-        int left=0;
-        int right=s.length()-1;
-        while(left<=right){
-            while( left<=right && !(s.charAt(left)>=65 && s.charAt(left)<=90 || 
-                  s.charAt(left)>=97 && s.charAt(left)<=122 ||
-                  s.charAt(left)>=48 && s.charAt(left)<=57)){
-                    left++;
-                  }
-             while( left<=right && !(s.charAt(right)>=65 && s.charAt(right)<=90 || 
-                  s.charAt(right)>=97 && s.charAt(right)<=122 ||
-                  s.charAt(right)>=48 && s.charAt(right)<=57)){
-                    right--;
-                  }
-            if(left<=right && Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-
-
-
-
-
-
-
-       /*
-        s=s.toLowerCase();
-        String res="";
+        
+        String lc="";
         for(int i=0;i<s.length();i++){
-            if((s.charAt(i)>=65 && s.charAt(i)<=90) ||
-              (s.charAt(i)>=97 && s.charAt(i)<=122) ||
-              (s.charAt(i)>=48 && s.charAt(i)<=57)){
-                    res=res+s.charAt(i);
-              }
+
+            char ch=s.charAt(i);
+
+            if(ch>='A' && ch<='Z'){
+                lc=lc+(char)(s.charAt(i)+32);
+            }
+            else if((ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
+                lc=lc+ch;
+            }
         }
-        int left=0,right=res.length()-1;
-        while(left<=right){
-            if(res.charAt(left)!=res.charAt(right)){
+
+
+       int left=0,right=lc.length()-1;
+
+       while(left<right){
+            if(lc.charAt(left)!=lc.charAt(right)){
                 return false;
             }
             left++;
             right--;
-        }
-        return true;*/
-
-
-
-
-
-
-
-
-
+       }
+       return true;
 
 
 
