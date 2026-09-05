014e6838d53a2399dd33cd6df7c36f36e6c85e15
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-05 10:24:44 +0530

    Time: 4 ms (83.62%) | Memory: 44.1 MB (89.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,33 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
-        int[] arr1=new int[26];
-        int[] arr2=new int[26];
+        
+        int[] arr=new int[26];
+
         for(int i=0;i<s.length();i++){
-            int index=s.charAt(i)-97;
-            arr1[index]++;
+            char ch=s.charAt(i);
+
+            int index=ch-97;
+            arr[index]++;
         }
+
+
         for(int i=0;i<t.length();i++){
-            int index=t.charAt(i)-97;
-            arr2[index]++;
+            char ch=t.charAt(i);
+
+            int index=ch-97;
+            arr[index]--;
         }
-        for(int i=0;i<26;i++){
-           if(arr1[i]!=arr2[i]){
-            return false;
-           }
+
+
+        for(int ele: arr){
+            if(ele!=0){
+                return false;
+            }
         }
         return true;
-       
+
+
+
+
     }
 }
