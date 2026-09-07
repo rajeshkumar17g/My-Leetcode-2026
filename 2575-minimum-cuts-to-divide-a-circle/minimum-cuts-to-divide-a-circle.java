e337f07f230240e2c9abf812dcc3ac2e12d3b112
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-07 11:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (22.98%) - LeetSync

--- 2575-minimum-cuts-to-divide-a-circle/minimum-cuts-to-divide-a-circle.java
+++ 2575-minimum-cuts-to-divide-a-circle/minimum-cuts-to-divide-a-circle.java
@@ -1,8 +1,13 @@
 class Solution {
     public int numberOfCuts(int n) {
-        if(n%2==0 || n==1){
+        if(n==1){
+            return 0;
+        }
+        else if(n%2==0){
             return n/2;
         }
-        return n;
+        else{
+            return n;
+        }
     }
 }
