95b7098af1772a71aa48f096fd6e32897e3793de
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-08 10:07:24 +0530

    Time: 2 ms (84.17%) | Memory: 42.4 MB (9.83%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,11 +1,14 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
         
-        int sum=0;
-        for(int i=1;i<=num/2;i++){
+        int sum=1;
+        for(int i=2;i<=Math.sqrt(num);i++){
             if(num%i==0){
-               sum=sum+i;
+               sum=sum+i+num/i;
             }
+        }
+        if((int)Math.sqrt(num)*(int)Math.sqrt(num)==num){
+            sum=sum-(int)Math.sqrt(num);
         }
 
         return sum==num;
