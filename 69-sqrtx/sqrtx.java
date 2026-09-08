ea275328a089e809940e1dc601c737a2f270970b
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-08 12:11:07 +0530

    Time: 1 ms (98.96%) | Memory: 42.8 MB (23.73%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,12 +1,32 @@
 class Solution {
     public int mySqrt(int x) {
-        long i;
+        
+        long low=0,high=x;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+
+            if(mid*mid==x){
+                return (int)mid;//perfect square
+            }
+            else if(mid*mid>x){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+
+        return (int)high;
+    }
+}
+
+/*
+
+long i;
         for(i=1;i<=x;i++){
             if(i*i>x){
                break;
             }
         }
 
-        return (int)i-1;
-    }
-}
+        return (int)i-1;*/
