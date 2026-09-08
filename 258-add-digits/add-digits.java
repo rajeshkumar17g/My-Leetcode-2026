eabe86b2b6f9578a697b73108af2e10896c910f6
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-08 09:39:59 +0530

    Time: 1 ms (98.07%) | Memory: 42.4 MB (90.76%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+
+        int sum=0;
+        while(num!=0){
+            int dig=num%10;
+            sum=sum+dig;
+            num=num/10;
+            if(num==0 && sum>9){
+                num=sum;
+                sum=0;
+            }
+        }
+
+        return sum;
+    }
+}
