ca00c9bf14aede7a8058cc794526c3c3286a37c3
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-09 14:44:34 +0530

    Time: 128 ms (5.45%) | Memory: 47.9 MB (7.55%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,59 +1,25 @@
 class Solution {
-    public boolean isalnum(char ch){
-        if(ch>='a' && ch<='z' || ch>='A' && ch<='Z' || ch>='0' && ch<='9'){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
     public boolean isPalindrome(String s) {
         
-
-        int left=0,right=s.length()-1;
-        while(left<right){
-
-            while(left<right && isalnum(s.charAt(left))==false){
-                left++;
-            }
-
-            while(left<right && isalnum(s.charAt(right))==false){
-                right--;
-            }
-
-            if(left<right && Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
-
-    }
-}
-
-
-/*
-
-        String lc="";
+        String res="";
         for(int i=0;i<s.length();i++){
-
             char ch=s.charAt(i);
+            //alphanumeric
+            if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9')){
 
-            if(ch>='A' && ch<='Z'){
-                lc=lc+(char)(s.charAt(i)+32);
-            }
-            else if((ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
-                lc=lc+ch;
+                if(ch>='A' && ch<='Z'){
+                    res=res+(char)(ch+32);
+                }
+                else{
+                    res=res+ch;
+                }
             }
         }
 
 
-       int left=0,right=lc.length()-1;
-
+       int left=0,right=res.length()-1;
        while(left<right){
-            if(lc.charAt(left)!=lc.charAt(right)){
+            if(res.charAt(left)!=res.charAt(right)){
                 return false;
             }
             left++;
@@ -61,4 +27,9 @@ class Solution {
        }
        return true;
 
-*/
+
+
+
+
+    }
+}
