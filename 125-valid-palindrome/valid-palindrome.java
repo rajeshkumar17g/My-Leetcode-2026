e22c72b4f2fbc7f67d1b560438e36c68eb231238
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-10 10:01:33 +0530

    Time: 2 ms (99.31%) | Memory: 44.3 MB (73.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,53 @@
 class Solution {
+    public boolean alnum(char ch){
+        if(ch>='a' && ch<='z' || ch>='A' && ch<='Z' || ch>='0' && ch<='9'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public char tolowercase(char ch){
+        if(ch>='A' && ch<='Z'){
+            return (char)(ch+32);
+        }
+        else{
+            return ch;
+        }
+    }
     public boolean isPalindrome(String s) {
         
-        String res="";
+        int left=0,right=s.length()-1;
+
+        while(left<right){
+
+            //left should be on an alnum
+            while(left<right && alnum(s.charAt(left))==false){
+                left++;
+            }
+
+             while(left<right && alnum(s.charAt(right))==false){
+                right--;
+            }
+
+            if(tolowercase(s.charAt(left))!=tolowercase(s.charAt(right))){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+
+
+
+    }
+}
+
+/*
+
+     String res="";
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             //alphanumeric
@@ -25,11 +71,4 @@ class Solution {
             left++;
             right--;
        }
-       return true;
-
-
-
-
-
-    }
-}
+       return true;*/
