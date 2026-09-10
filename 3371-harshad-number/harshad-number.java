ee0a6c75ba11df42742e13f6d3f0a7a4e8a9c597
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-10 19:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (8.88%) - LeetSync

--- 3371-harshad-number/harshad-number.java
+++ 3371-harshad-number/harshad-number.java
@@ -1,14 +1,14 @@
 class Solution {
     public int sumOfTheDigitsOfHarshadNumber(int x) {
         
-        int dup=x;
         int sum=0;
+        int dup=x;
         while(x!=0){
             int dig=x%10;
             sum=sum+dig;
             x=x/10;
         }
-
+       
         if(dup%sum==0){
             return sum;
         }
