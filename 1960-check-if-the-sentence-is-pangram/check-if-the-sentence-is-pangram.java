9340ccf63b112f6a479b93371e57cb70cf99343a
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-10 10:42:08 +0530

    Time: 2 ms (62.89%) | Memory: 43.1 MB (33.56%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -3,18 +3,21 @@ class Solution {
         
         for(int ascii=97;ascii<=122;ascii++){
             char ch=(char)ascii;
+           
             int flag=0;
             for(int i=0;i<s.length();i++){
-                if(s.charAt(i)==ch){
+                if(ch==s.charAt(i)){
                     flag=1;
                     break;
                 }
             }
+
             if(flag==0){
                 return false;
             }
-
         }
+
+
         return true;
     }
 }
