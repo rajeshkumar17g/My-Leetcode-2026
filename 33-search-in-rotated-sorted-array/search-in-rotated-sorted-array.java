4d1777ca60a60e358901440bc0c423844a0eeda5
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-13 14:38:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (65.79%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,11 +1,50 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-     for(int i=0;i<nums.length;i++){
+        int low=0,high=nums.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(target==nums[mid]){
+                return mid;
+            }
+
+            //check if left is sorted???
+            else if(nums[low]<=nums[mid]){
+                //if ele is in left side?
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+
+            }
+            //else right is sorted
+            else{
+                if(target>nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+                
+            }
+        }
+
+        return -1;
+   
+    }
+}
+
+
+/*
+  for(int i=0;i<nums.length;i++){
         if(nums[i]==target){
             return i;
         }
      }   
      return -1;
-    }
-}
+
+*/
