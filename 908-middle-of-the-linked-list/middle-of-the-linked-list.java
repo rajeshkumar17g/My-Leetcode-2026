9b8c43697fb70ca9097655948f0ed847be05a713
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-21 19:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (71.54%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -15,7 +15,8 @@ class Solution {
 
       while(fast!=null && fast.next!=null){
             slow=slow.next;
-            fast=fast.next.next;
+            fast=fast.next;
+            fast=fast.next;
       }
 
       return slow;
