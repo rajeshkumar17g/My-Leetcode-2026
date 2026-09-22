046613c99debad1534d4d4acae7fd41389d8fcb7
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-22 15:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (71.21%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,51 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
-        //first occurance
-        int fo=-1;
-        int low=0,high=nums.length-1;
 
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(target==nums[mid]){
-                fo=mid;
-                high=mid-1;
-            }
-            else if(target>nums[mid]){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+     int low=0,high=nums.length-1;
+
+     int fo=-1;
+
+     while(low<=high){
+        int mid=low+(high-low)/2;
+
+        if(target==nums[mid]){
+            fo=mid;
+            high=mid-1;
         }
-
-
-
-
-        //last occurance
-          //first occurance
-        int lo=-1;
-        low=0;
-        high=nums.length-1;
-
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(target==nums[mid]){
-                lo=mid;
-                low=mid+1;
-            }
-            else if(target>nums[mid]){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+        else if(target>nums[mid]){
+            low=mid+1;
         }
+        else{
+            high=mid-1;
+        }
+     }
 
-        return new int[]{fo,lo};
+
+
+     low=0;
+     high=nums.length-1;
+
+     int lo=-1;
+
+     while(low<=high){
+        int mid=low+(high-low)/2;
+
+        if(target==nums[mid]){
+            lo=mid;
+            low=mid+1;
+        }
+        else if(target>nums[mid]){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+     }
+
+     return new int[] {fo,lo};
+
+
+
+
     }
 }
