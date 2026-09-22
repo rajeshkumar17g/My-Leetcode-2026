9c4fa8025f7c1bfe6316d4351a7b9d28b7649d12
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-22 19:51:15 +0530

    Time: 3 ms (99.82%) | Memory: 94.7 MB (35.88%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,50 +1,29 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * 
- 
- 
- 
- }
 
-1. middle 
-2. 1st half reverse upto middle
-3. check 1st half and 2nd half
-
-Note: fast!=null=> odd number => skip mid for crr2 
-
-
-
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        
-        ListNode slow=head,fast=head;
-
-        while(fast!=null && fast.next!=null){
+       
+       ListNode slow=head,fast=head;
+       while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-        }
-        
-        ListNode rev_head=null,after,crr=head;
+       } 
 
-        while(crr!=slow){
+       ListNode crr=head,after,rev_head=null;
+       while(crr!=slow){
             after=crr.next;
             crr.next=rev_head;
             rev_head=crr;
             crr=after;
-        }
+       }
 
-        if(fast!=null){
-            slow=slow.next;
-        }
+       // 
+       ListNode crr1=rev_head;
+       ListNode crr2=slow;
+       //odd number of nodes
+       if(fast!=null){
+         crr2=slow.next;
+       }
 
-       ListNode crr1=rev_head,crr2=slow;
        while(crr1!=null && crr2!=null){
             if(crr1.val!=crr2.val){
                 return false;
@@ -53,11 +32,6 @@ class Solution {
             crr2=crr2.next;
        }
        return true;
-
-
-
-
-
 
     }
 }
