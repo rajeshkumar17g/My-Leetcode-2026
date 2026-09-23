d3d39e2424579d6ea3a4c6b4c570bfa1a4d3b198
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-23 14:37:47 +0530

    Time: 1 ms (77.85%) | Memory: 42.3 MB (78.41%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,6 +1,37 @@
 class Solution {
+    public int sod(int n){
+            int sum=0;
+            while(n!=0){
+                int dig=n%10;
+                sum=sum+dig*dig;
+                n=n/10; 
+            }
+            return sum;
+    }
     public boolean isHappy(int n) {
-       HashSet<Integer> seen=new HashSet<>();
+      
+      if(n==1){
+        return true;
+      }
+      int slow=n;
+      int fast=sod(n);
+      while(slow!=fast){
+        slow=sod(slow);      //1 step
+        fast=sod(sod(fast)); // 2 steps
+        //System.out.println("SLow: "+slow+" Fast: "+fast);
+        if(slow==1 || fast==1){
+            return true;
+        }
+      }
+      return false;
+    
+
+    }
+}
+
+/*
+
+ HashSet<Integer> seen=new HashSet<>();
 
         while(true){
 
@@ -21,6 +52,4 @@ class Solution {
             n=sum;
         }
 
-
-    }
-}
+*/
