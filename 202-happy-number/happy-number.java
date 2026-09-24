61bbb28c6ff225cda400afa36d49580983c64863
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-24 10:14:45 +0530

    Time: 14 ms (1.87%) | Memory: 42.7 MB (21.43%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,55 +1,32 @@
 class Solution {
     public int sod(int n){
-            int sum=0;
-            while(n!=0){
-                int dig=n%10;
-                sum=sum+dig*dig;
-                n=n/10; 
-            }
-            return sum;
+
+        int sum=0;
+        while(n!=0){
+            int dig=n%10;
+            sum=sum+dig*dig;
+            n=n/10;
+        }
+
+        return sum;
     }
     public boolean isHappy(int n) {
-      
-      if(n==1){
-        return true;
-      }
-      int slow=n;
-      int fast=sod(n);
-      while(slow!=fast){
-        slow=sod(slow);      //1 step
-        fast=sod(sod(fast)); // 2 steps
-        //System.out.println("SLow: "+slow+" Fast: "+fast);
-        if(slow==1 || fast==1){
+        if(n==1){
             return true;
         }
-      }
-      return false;
-    
+        int slow=n;
+        int fast=sod(n);
+        
+        while(fast!=slow){
+            slow=sod(slow);
+            fast=sod(sod(fast));
+             System.out.println("Slow: "+slow+" fast: "+fast);
 
+            if(slow==1 || fast==1){
+                return true;
+            }
+           
+        }
+        return false;
     }
 }
-
-/*
-
- HashSet<Integer> seen=new HashSet<>();
-
-        while(true){
-
-            int sum=0;
-            while(n!=0){
-                int dig=n%10;
-                sum=sum+dig*dig;
-                n=n/10;
-                
-            }
-            if(sum==1){
-                return true;  
-            }
-            if(seen.contains(sum)==true){
-                return false;
-            }
-            seen.add(sum);
-            n=sum;
-        }
-
-*/
