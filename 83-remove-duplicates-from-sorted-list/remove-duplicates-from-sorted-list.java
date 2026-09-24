506043cdfc9ee8e5ecb553e23ae59205f63410e5
Rajesh Kumar <rajeshkumar17g@gmail.com>
2026-09-24 19:47:06 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (32.83%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -1,27 +1,26 @@
 
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-
+        
         if(head==null || head.next==null){
             return head;
         }
 
-        ListNode prev=head,crr=head.next;
-
+        ListNode prev=head;
+        ListNode crr=head.next;
         while(crr!=null){
             if(prev.val==crr.val){
                 crr=crr.next;
             }
             else{
                 prev.next=crr;
+                prev=crr;
                 crr=crr.next;
-                prev=prev.next;
             }
         }
+
         prev.next=null;
 
         return head;
-
-
     }
 }
